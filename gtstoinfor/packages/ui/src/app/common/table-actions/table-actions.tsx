716317c0102caeb67b74ebc19403edafd0179bd3
dileepraghumajji88 <dileepraghumajji88@gmail.com>
2023-06-29 11:06:51 +0530

    factories view changes

---
+++ gtstoinfor/packages/ui/src/app/common/table-actions/table-actions.tsx
@@ -0,0 +1,21 @@
+import { EditOutlined } from '@ant-design/icons';
+import { Divider, Popconfirm, Switch } from 'antd'
+import React from 'react';
+
+type TableActionsType = {
+    isActive : boolean,
+    onEditClick : () => void,
+    onSwitchClick : () => void
+}
+
+export default function TableActions(props : TableActionsType) {
+  return (
+   <span>
+        <Popconfirm title={`Are you sure to ${props.isActive ? 'Deactivate' : 'Activate'}`}>
+            <Switch size='small' checked={!props.isActive}  onClick={props.onSwitchClick} />
+        </Popconfirm>
+        <Divider type='vertical' />
+        <EditOutlined  onClick={props.onEditClick} />
+   </span>
+  )
+}
--- gtstoinfor/packages/ui/src/app/masters/factories/factories-view.tsx
+++ gtstoinfor/packages/ui/src/app/masters/factories/factories-view.tsx
@@ -1,43 +1,83 @@
 import { EditOutlined } from '@ant-design/icons'
-import { ProTable } from '@ant-design/pro-components'
+import { ProColumns, ProTable } from '@ant-design/pro-components'
 import { FactoryDto } from '@project-management-system/shared-models'
 import { FactoryService } from '@project-management-system/shared-services'
-import { Button, Space, Tag } from 'antd'
-import {forEachObject} from 'for-each'
+import { Button, Popconfirm, Space, Tag } from 'antd'
+import { forEachObject } from 'for-each'
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import TableActions from '../../common/table-actions/table-actions'
 
 export default function FactoriesView() {
   const navigate = useNavigate()
-  const [factoriesData,setFactoriesData] = useState<FactoryDto[]>()
+  const [factoriesData, setFactoriesData] = useState<FactoryDto[]>()
   const factoryService = new FactoryService()
 
-  useEffect(() => {
-    getData()
-  },[])
 
-  function renderActions(){
-    <span>
-      
-    </span>
+
+  function onEditClick() {
+
   }
-  const getData = () => {
-      factoryService.getFactories().then((res) => {
-        if(res.status){
-          setFactoriesData(res.data)
-        }
-      })
+
+  function onSwitchClick() {
+
   }
-  const  columns = [
-    {title:'Factory name',dataIndex:'name'},
-    {title:'Address',dataIndex:"address"},
-    {title:'Status',render:(val) => {return <Tag color={val ? 'green' : 'red'}>{val ? 'Active' : 'Inactive'}</Tag>}},
-    {title:'Action',render:(val) => {return <span></span>}}
+  const getData = async (params = {}, sort, filter) => {
+    const res = await factoryService.getFactories()
+    if (res.status) {
+      return { data: res.data, sucess: true, total: res.data.length }
+    } else {
+      return { data: [], sucess: false, total: 0 }
+    }
+  }
+
+  const columns: ProColumns<FactoryDto>[] = [
+    {
+      dataIndex: 'index',
+      valueType: 'indexBorder',
+      width: 48,
+      align:'center'
+    },
+    { title: 'Factory name', dataIndex: 'name', sorter: true },
+    { title: 'Address', dataIndex: "address", },
+    {
+      title: 'Status',
+      filters: true,
+      onFilter: true,
+      ellipsis: true,
+      valueType: 'select',
+      valueEnum: {
+        open: {
+          text: 'Active',
+          status: 'Error',
+        },
+        closed: {
+          text: 'Inactive',
+          status: 'Success',
+        },
+      },
+      render: (dom, entity) => { return <Tag color={entity.isActive ? 'green' : 'red'}>{entity.isActive ? 'Active' : 'Inactive'}</Tag> }
+    },
+    { title: 'Action', render: (dom, entity) => { return <TableActions isActive={entity.isActive} onEditClick={onEditClick} onSwitchClick={onSwitchClick} /> } }
   ]
   forEachObject
   return (
-   
-      <ProTable<FactoryDto> dataSource={factoriesData} size='small' cardBordered   cardProps={{extra:<span><Button onClick={() => navigate('/masters/factories/factories-form')} type={'primary'}>New</Button></span>}}  search={false} headerTitle={'Factories'} columns={columns} />
-   
+
+    <ProTable<FactoryDto, any>
+      request={getData}
+      bordered size='small'
+      cardBordered
+      editable={{
+        type: 'multiple',
+      }}
+      cardProps={{
+        extra: <span><Button onClick={() => navigate('/masters/factories/factories-form')}
+          type={'primary'}>New</Button></span>
+      }}
+      search={false} headerTitle={'Factories'}
+      columns={columns}
+
+    />
+
   )
 }
