b19960cc324ac54b40cdd93af47351bed1159aed
saiprakashkatiki <saiprakash@schemaxtech.com>
2023-07-07 13:22:00 +0530

    query changes

--- gtstoinfor/packages/services/common/src/app/orders/repository/orders-child.repository.ts
+++ gtstoinfor/packages/services/common/src/app/orders/repository/orders-child.repository.ts
@@ -1,11 +1,14 @@
-import { Repository, getManager } from "typeorm";
+import { Connection, Repository, getManager } from "typeorm";
 import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
+import { InjectConnection, InjectRepository } from "@nestjs/typeorm";
 import { OrdersChildEntity } from "../orders-child.entity";
+import { OrdersEntity } from "../orders.entity";
 
 @Injectable()
 export class OrdersChildRepository extends Repository<OrdersChildEntity> {
-    constructor(@InjectRepository(OrdersChildEntity) private orderChildRepository: Repository<OrdersChildEntity>
+    constructor(@InjectRepository(OrdersChildEntity)
+    private orderChildRepository: Repository<OrdersChildEntity>
+        // @InjectConnection() private readonly connection: Connection
     ) {
         super(orderChildRepository.target, orderChildRepository.manager, orderChildRepository.queryRunner);
     }
@@ -25,11 +28,19 @@ export class OrdersChildRepository extends Repository<OrdersChildEntity> {
         return data;
     }
 
-    async getQtyChangeData(productionPlanId: string): Promise<any> {
-        const manager = getManager()
-        const query = `SELECT t1.id AS id, t1.production_plan_id AS production_plan_id, t1.order_qty_pcs AS original_column1, t2.order_qty_pcs AS new_column1, t1.requested_wh_date AS original_column2 FROM orders_child t1 JOIN orders_child t2 ON t1.production_plan_id = t2.production_plan_id WHERE t1.order_qty_pcs <> t2.order_qty_pcs AND t1.production_plan_id = '${productionPlanId}'  ORDER BY t1.created_at DESC LIMIT 1`
-        const data = await manager.query(query);
-        return data;
+    // async getQtyChangeData(): Promise<any[]> {
+    //     const manager = getManager('')
+    //     const query = `SELECT oc1.production_plan_id, GROUP_CONCAT(oc1.order_qty_pcs ORDER BY oc1.version) AS order_qty_pieces FROM orders_child oc1 INNER JOIN  orders_child oc2 ON oc1.production_plan_id=oc2.production_plan_id WHERE oc1.order_qty_pcs <> oc2.order_qty_pcs AND oc1.order_qty_pcs IS NOT NULLGROUP BY oc1.production_plan_id`
+    //     const data = await manager.query(query);
+    //     return data;
+    // }
+
+    async getQtyChangeData(): Promise<any[]> {
+        const query = this.createQueryBuilder('oc')
+            .select(`oc.production_plan_id, oc.item_name, oc.contract_date,oc.requested_wh_date, od.created_at, od.old_val, od.new_val, od.version`)
+            .leftJoin(OrdersEntity, 'od', 'od.production_plan_id = oc.prod_plan_id')
+            .where(` column_name='order_qty_pcs' ORDER BY od.created_at DESC LIMIT 1`)
+        return await query.getRawMany();
     }
 
     async getWharehouseDateChangeData(productionPlanId: string): Promise<any> {
--- gtstoinfor/packages/ui/src/app/excel-import/changes-grid.tsx
+++ gtstoinfor/packages/ui/src/app/excel-import/changes-grid.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { Card, Col, Form, Row, Table, Tabs, TabsProps, Tag } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Card, Col, DatePicker, Form, Row, Select, Table, Tabs, TabsProps, Tag } from 'antd';
 import TabPane from 'antd/es/tabs/TabPane';
+import { OrdersService } from '@project-management-system/shared-services';
 
 const ChangesGrid = () => {
-    const dataSource = [
-        {
-            'S.No': '1',
-            'productionPlanId': '123',
-            'Column 1': '100',
-            'Column 1 Changed': '150',
-            'Column 2': 'Yes',
-            'Column 2 Changed': 'No',
-        },
-        {
-            'S.No': '2',
-            'productionPlanId': '123',
-            'Column 1': '150',
-            'Column 1 Changed': '200',
-            'Column 2': 'Done',
-            'Column 2 Changed': 'Not',
-        },
-    ];
+    const service = new OrdersService();
+    const [qtyData, setQtyData] = useState<any[]>([])
+    const [reqWhrDateData, setReqWhrDateData] = useState<any[]>([])
+    const [contDateData, setContDateData] = useState<any[]>([])
+    const { RangePicker } = DatePicker
+
+    useEffect(() => {
+        getQtyChangedData()
+    }, [])
+
+    const getQtyChangedData = () => {
+        service.getQtyChangeData().then(res => {
+            if (res.status) {
+                setQtyData(res.data)
+            }
+        })
+    }
 
     const columns = [
         {
@@ -41,34 +41,119 @@ const ChangesGrid = () => {
             children: [
                 {
                     title: 'Previous Value',
-                    dataIndex: 'Column 1',
-                    key: 'Column 1',
+                    dataIndex: 'oldValue',
+                    key: 'oldValue',
                 },
                 {
                     title: 'Revised Value',
-                    dataIndex: 'Column 1 Changed',
-                    key: 'Column 1 Changed',
+                    dataIndex: 'newValue',
+                    key: 'newValue',
                 },
             ],
         },
-        
+        {
+            title: 'Contract Date',
+            dataIndex: 'contractDate'
+        },
+        {
+            title: 'Requested Warehouse Date',
+            dataIndex: 'requestedWarehuse Date'
+        }
+    ];
+
+    const columns1 = [
+        {
+            title: 'S.No',
+            dataIndex: 'S.No',
+            key: 'S.No',
+        },
+        {
+            title: 'Production plan id',
+            dataIndex: 'productionPlanId'
+        },
+        {
+            title: 'Item code',
+            dataIndex: 'itemCode'
+        },
+        {
+            title: 'Requested Warehouse Date',
+            children: [
+                {
+                    title: 'Previous Value',
+                    dataIndex: 'oldValue',
+                    key: 'oldValue',
+                },
+                {
+                    title: 'Revised Value',
+                    dataIndex: 'newValue',
+                    key: 'newValue',
+                },
+            ],
+        },
+        {
+            title: 'Contract Date',
+            dataIndex: 'contractDate'
+        },
+        {
+            title: 'Quantity',
+            dataIndex: 'orderQtyPcs'
+        }
+    ];
+
+    const columns2 = [
+        {
+            title: 'S.No',
+            dataIndex: 'S.No',
+            key: 'S.No',
+        },
+        {
+            title: 'Production plan id',
+            dataIndex: 'productionPlanId'
+        },
+        {
+            title: 'Item code',
+            dataIndex: 'itemCode'
+        },
+        {
+            title: 'Contract Date',
+            children: [
+                {
+                    title: 'Previous Value',
+                    dataIndex: 'oldValue',
+                    key: 'oldValue',
+                },
+                {
+                    title: 'Revised Value',
+                    dataIndex: 'newValue',
+                    key: 'newValue',
+                },
+            ],
+        },
+        {
+            title: 'Quantity',
+            dataIndex: 'orderQtyPcs'
+        },
+        {
+            title: 'Requested Warehouse Date',
+            dataIndex: 'requestedWarehuse Date'
+        }
     ];
 
     const items: TabsProps['items'] = [
         {
             key: '1',
-            label: <Tag color='blue'>Column 1</Tag>,
-            children: <Table dataSource={dataSource} columns={columns} />,
+            label: 'Order Quantity',
+            children: <Table dataSource={qtyData} columns={columns} />,
         },
         {
             key: '2',
-            label: <Tag color='blue'>Column 1</Tag>,
-            children: <Table dataSource={dataSource} columns={columns} />,
+            label: 'Requested Warehouse Date',
+            children: <Table dataSource={reqWhrDateData} columns={columns1} />,
         },
         {
             key: '3',
-            label: <Tag color='blue'>Column 1</Tag>,
-            children: <Table dataSource={dataSource} columns={columns} />,
+            label: 'Contract Date',
+            children: <Table dataSource={contDateData} columns={columns2} />,
         },
     ];
 
@@ -77,23 +162,25 @@ const ChangesGrid = () => {
             <Form layout={"vertical"} >
                 <Row gutter={[24, 24]}>
                     <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6 }} lg={{ span: 6 }} xl={{ span: 6 }}>
-                        <Form.Item name=""
-                            label=""
+                        <Form.Item name="contractDate"
+                            label="Contract Date"
                         >
+                            <RangePicker />
                         </Form.Item>
                     </Col>
                     <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6 }} lg={{ span: 6 }} xl={{ span: 6 }}>
+                        <Form.Item name="orderStatus"
+                            label="Order Status"
+                        >
+                            <Select></Select>
+                        </Form.Item>
+                    </Col>
+                    {/* <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6 }} lg={{ span: 6 }} xl={{ span: 6 }}>
                         <Form.Item name=""
                             label=""
                         >
                         </Form.Item>
-                    </Col>
-                    <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6 }} lg={{ span: 6 }} xl={{ span: 6 }}>
-                        <Form.Item name=""
-                            label=""
-                        >
-                        </Form.Item>
-                    </Col>
+                    </Col> */}
                 </Row>
             </Form>
             <Tabs items={items} />
