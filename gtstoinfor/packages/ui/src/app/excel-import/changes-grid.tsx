be989440288185e23db8071a40b54353ae89a419
saiprakashkatiki <saiprakash@schemaxtech.com>
2023-07-07 22:18:11 +0530

    UI changes

--- gtstoinfor/packages/ui/src/app/excel-import/changes-grid.tsx
+++ gtstoinfor/packages/ui/src/app/excel-import/changes-grid.tsx
@@ -8,8 +8,14 @@ const ChangesGrid = () => {
     const [contractDateData, setContractDateData] = useState([])
     const [qtyData, setQtyData] = useState([])
     const [warehouseDateData, setWarehouseDateDate] = useState([])
+    const [filteredContractDateData, setFilteredContractDateData] = useState([])
+    const [filteredQtyData, setFilteredQtyData] = useState([])
+    const [filteredWarehouseDateData, setFilteredWarehouseDateDate] = useState([])
+    const [selectedEstimatedFromDate, setSelectedEstimatedFromDate] = useState(undefined);
+    const [selectedEstimatedToDate, setSelectedEstimatedToDate] = useState(undefined);
     const [pageSize, setPageSize] = useState<number>(null);
     const [page, setPage] = React.useState(1);
+    const [form] = Form.useForm();
     const { RangePicker } = DatePicker
     const { Option } = Select
 
@@ -22,23 +28,23 @@ const ChangesGrid = () => {
     const getContractDateChangeData = () => {
         service.getContractDateChangeData().then((res) => {
             setContractDateData(res.data)
+            setFilteredContractDateData(res.data)
         })
     }
 
     const getQtyChangeData = () => {
         service.getQtyChangeData().then((res) => {
             setQtyData(res.data)
-
+            setFilteredQtyData(res.data)
         })
     }
 
     const getWharehouseDateChangeData = () => {
         service.getWharehouseDateChangeData().then((res) => {
             setWarehouseDateDate(res.data)
+            setFilteredWarehouseDateDate(res.data)
         })
     }
-
-    console.log(contractDateData)
 
     const columns = [
         {
@@ -76,6 +82,10 @@ const ChangesGrid = () => {
         {
             title: 'Requested Warehouse Date',
             dataIndex: 'requested_wh_date'
+        },
+        {
+            title: 'Color Code',
+            dataIndex: 'color_code'
         },
         {
             title: 'Order Status',
@@ -119,6 +129,10 @@ const ChangesGrid = () => {
         {
             title: 'Contract Date',
             dataIndex: 'contracted_date'
+        },
+        {
+            title: 'Color Code',
+            dataIndex: 'color_code'
         },
         {
             title: 'Order Status',
@@ -165,38 +179,86 @@ const ChangesGrid = () => {
             dataIndex: 'requested_wh_date'
         },
         {
+            title: 'Color Code',
+            dataIndex: 'color_code'
+        },
+        {
             title: 'Order Status',
             dataIndex: 'order_status'
         }
     ];
 
+    const EstimatedETDDate = (value) => {
+        if (value) {
+            console.log(value)
+            const fromDate = value[0].format('YYYY-MM-DD');
+            const toDate = value[1].format('YYYY-MM-DD');
+            setSelectedEstimatedFromDate(fromDate)
+            setSelectedEstimatedToDate(toDate)
+        }
+    }
+
+    const getFilterdData = () => {
+        let orderStatus = form.getFieldValue('orderStatus');
+        let startDate = selectedEstimatedFromDate;
+        let endDate = selectedEstimatedToDate;
+        let filteredContractData = contractDateData;
+        let filteredQtyData = qtyData
+        let filteredReqWhData = warehouseDateData
+        if (orderStatus) {
+            filteredContractData = filteredContractData.filter(record => record.order_status === orderStatus);
+            filteredQtyData = filteredQtyData.filter(record => record.order_status === orderStatus)
+            filteredReqWhData = filteredReqWhData.filter(record => record.order_status === orderStatus)
+            setFilteredContractDateData(filteredContractData);
+            setFilteredQtyData(filteredQtyData)
+            setFilteredWarehouseDateDate(filteredReqWhData)
+        }
+        if (startDate && endDate) {
+            filteredContractData = filteredContractData.filter(record => record.contracted_date >= startDate && record.contracted_date <= endDate);
+            filteredQtyData = filteredQtyData.filter(record => record.contracted_date >= startDate && record.contracted_date <= endDate)
+            filteredReqWhData = filteredReqWhData.filter(record => record.contracted_date >= startDate && record.contracted_date <= endDate)
+            setFilteredContractDateData(filteredContractData);
+            setFilteredQtyData(filteredQtyData)
+            setFilteredWarehouseDateDate(filteredReqWhData)
+        }
+    }
+
     const items: TabsProps['items'] = [
         {
             key: '1',
-            label: <b>Order Qty : {qtyData?.length} </b>,
-            children: <Table dataSource={qtyData} columns={columns} />,
+            label: <b>Order Qty : {filteredQtyData?.length} </b>,
+            children: <Table dataSource={filteredQtyData} columns={columns} />,
         },
         {
             key: '2',
-            label: <b >Requested Warehouse Date : {warehouseDateData?.length}</b>,
-            children: <Table dataSource={warehouseDateData} columns={columns1} />,
+            label: <b >Requested Warehouse Date : {filteredWarehouseDateData?.length}</b>,
+            children: <Table dataSource={filteredWarehouseDateData} columns={columns1} />,
         },
         {
             key: '3',
-            label: <b>Contract date : {contractDateData?.length}</b>,
-            children: <Table dataSource={contractDateData} columns={columns2} />,
+            label: <b>Contract date : {filteredContractDateData?.length}</b>,
+            children: <Table dataSource={filteredContractDateData} columns={columns2} />,
         },
     ];
 
+    const onReset = () => {
+        form.resetFields();
+        setSelectedEstimatedFromDate(undefined);
+        setSelectedEstimatedToDate(undefined);
+        getContractDateChangeData()
+        getQtyChangeData()
+        getWharehouseDateChangeData()
+    }
+
     return (
         <Card title='Revised orders'>
-            <Form layout={"vertical"} >
+            <Form form={form} layout={"vertical"} >
                 <Row gutter={[24, 24]}>
                     <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6 }} lg={{ span: 6 }} xl={{ span: 6 }}>
                         <Form.Item name="contractDate"
                             label="Contract Date"
                         >
-                            <RangePicker />
+                            <RangePicker onChange={EstimatedETDDate} />
                         </Form.Item>
                     </Col>
                     <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6 }} lg={{ span: 6 }} xl={{ span: 6 }}>
@@ -207,13 +269,16 @@ const ChangesGrid = () => {
                                 allowClear
                                 placeholder='select order status'
                             >
-                                <Option key='new' value="new">New</Option>
-                                <Option key='unaccepted' value="unaccepted">Unaccepted</Option>
+                                <Option key='new' value="NEW">NEW</Option>
+                                <Option key='unaccepted' value="UNACCEPTED">UNACCEPTED</Option>
                             </Select>
                         </Form.Item>
                     </Col>
+                    <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6 }} lg={{ span: 6 }} xl={{ span: 2 }}>
+                        <Button type="primary" style={{ marginTop: '25px' }} onClick={getFilterdData}>Search</Button>
+                    </Col>
                     <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6 }} lg={{ span: 6 }} xl={{ span: 6 }}>
-                        <Button type="primary" style={{ marginTop: '25px' }}>Search</Button>
+                        <Button onClick={onReset} style={{ marginTop: '25px' }}>Reset</Button>
                     </Col>
                 </Row>
             </Form>
