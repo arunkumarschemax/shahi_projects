f2f14135e5cc2a9091c83fcecc64b1375b3fe6ac
gopalsfdc08 <kothapalli.gopal@gmail.com>
2023-07-31 11:39:51 +0530

    supplier frontend

--- gtstoinfor/packages/libs/shared-models/src/common/supplier/supplier-create-dto.ts
+++ gtstoinfor/packages/libs/shared-models/src/common/supplier/supplier-create-dto.ts
@@ -1,4 +1,5 @@
-export declare class SupplierCreateDto{
+export  class SupplierCreateDto{
+    id: number;
     category: string;
     supplierCode: number;  
     supplierName: string;  
@@ -18,7 +19,10 @@ export declare class SupplierCreateDto{
     contactNumber: number;
     email: string;
     creditPaymentPeriod: string;
-    constructor(
+    createdUser: string;
+    isActive?:boolean
+    versionFlag?: number;
+    constructor(id: number,
     category: string,
     supplierCode: number, 
     supplierName: string,  
@@ -38,5 +42,33 @@ export declare class SupplierCreateDto{
     contactNumber: number,
     email: string,
     creditPaymentPeriod: string,
-    )
+    createdUser:string,
+    isActive?:boolean,
+    versionFlag?: number)  {
+        this.id = id;
+        this.category= category;
+        this.supplierCode = supplierCode;
+        this.supplierName = supplierName;
+        this.GstNumber = GstNumber;
+        this.contactPerson = contactPerson;
+        this.street = street;
+        this.apartment = apartment;
+        this.city = city;
+        this.state = state;
+        this.district = district;
+        this.postalCode = postalCode;
+        this.commision = commision;
+        this.bankAccountNo = bankAccountNo;
+        this.bankIFSC = bankIFSC;
+        this.bankName = bankName;
+        this.bankBranch = bankBranch;
+        this.contactNumber = contactNumber;
+        this.email = email;
+        this.creditPaymentPeriod = creditPaymentPeriod;
+        this.createdUser = createdUser;
+        this.isActive = isActive;
+        this.versionFlag = versionFlag;
+    }
+    
+    
 }
