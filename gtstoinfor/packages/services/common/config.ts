32b3812afb72de39f41047daa2efb48bdced2369
saiprakashkatiki <saiprakash@schemaxtech.com>
2023-08-01 15:07:02 +0530

    config file

--- gtstoinfor/packages/services/common/config.ts
+++ gtstoinfor/packages/services/common/config.ts
@@ -6,12 +6,12 @@ export const appConfig = {
     },
     database: {
         type: process.env[`APP_DB_TYPE`] || 'mysql',
-        host: process.env[`APP_DB_HOST`] || '206.189.138.212',
+        host: process.env[`APP_DB_HOST`] || '165.22.220.143',
         port: parseInt(process.env[`APP_DB_PORT`]) || 3306,
-        username: process.env[`APP_DB_USER`] || 'sa_dev_user',
+        username: process.env[`APP_DB_USER`] || 'ramakrishna',
         password: process.env[`APP_DB_PASS`] || 'Schemax@23',
         dbName: process.env[`APP_DB_DBNAME`] || 'shahi_orders',
         poolLimit: parseInt(process.env[`APP_DB_POOL_LIMIT`]) || 50
     },
-    
+
 }
