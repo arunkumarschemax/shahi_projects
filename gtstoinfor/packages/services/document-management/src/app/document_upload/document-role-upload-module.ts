66334d8e95ea13d18b9e7b57fa6adbfa155d5a8e
Renukaattada <renukaattata21@gmail.com>
2023-08-11 11:57:59 +0530

    #error

--- gtstoinfor/packages/services/document-management/src/app/document_upload/document-role-upload-module.ts
+++ gtstoinfor/packages/services/document-management/src/app/document_upload/document-role-upload-module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { DocumentService } from './document.service';
 import { DocumentEntity } from './entities/documents.entity';
-import { DocumentController } from './document_upload.controller';
+// import { DocumentController } from './document_upload.controller';
 import { RoleEntity } from './entities/document-role-entity';
 import { RoleController } from './document-role-mapping-controller';
 import { RoleService } from './document_role_mapping.service';
