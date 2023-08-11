ca7c08a498c64f1c82a6673680d37ce7377cd648
dileepraghumajji88 <dileepraghumajji88@gmail.com>
2023-08-11 16:56:43 +0530

    m3 generic service

---
+++ gtstoinfor/packages/libs/shared-services/src/common/m3-geenric-service.ts
@@ -0,0 +1,32 @@
+import { m3Config } from "../../config";
+import axios from 'axios';
+const https = require('https');
+
+interface M3ApiArgs {
+    key: string
+    value: string
+}
+
+export class M3GenericService {
+    private auth = 'Basic ' + Buffer.from(`${m3Config.USER_NAME}:${m3Config.PASSWORD}`).toString('base64');
+    private headersRequest = {
+        Authorization: `${this.auth}`,
+    };
+    private agent = new https.Agent({
+        rejectUnauthorized: false,
+    });
+
+    async callM3Api(program: string, api: string, args: M3ApiArgs[]) {
+        try {
+            var baseUrl = 'https://172.17.3.115:23005/m3api-rest/execute/'
+            let urlWithArgs = baseUrl + '/' + program + api + '?'
+            for (const arg of args) {
+                urlWithArgs += urlWithArgs + `${arg.key = arg.value}&`
+            }
+            const response = await axios.get(urlWithArgs, { headers: this.headersRequest, httpsAgent: this.agent });
+            return response
+        } catch (err) {
+            return err
+        }
+    }
+}
