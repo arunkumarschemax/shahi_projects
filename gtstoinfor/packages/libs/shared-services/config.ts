ba6e236a940e82dff38b5e5223d5a95de3d5397d
manisha1712 <manisha.pilla@schemaxtech.com>
2023-08-11 13:30:32 +0530

    url commented

--- gtstoinfor/packages/libs/shared-services/config.ts
+++ gtstoinfor/packages/libs/shared-services/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    APP_PROJECT_MANAGEMENT_URL: "http://165.22.220.143:8001/api",
+    // APP_PROJECT_MANAGEMENT_URL: "http://165.22.220.143:8001/api",
     whatsapp_redirection_url: 'http://dev.schemaxtech.in/#',
     whatsapp_notification_url: 'http://206.189.138.212:3000',
     whatsapp_broadcast_url: 'http://206.189.138.212:3232/api',
