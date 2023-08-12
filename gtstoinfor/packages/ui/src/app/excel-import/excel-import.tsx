7115c848092023efbaae668ab53566d8fc004e94
manisha1712 <manisha.pilla@schemaxtech.com>
2023-08-12 13:12:48 +0530

    done

--- gtstoinfor/packages/ui/src/app/excel-import/excel-import.tsx
+++ gtstoinfor/packages/ui/src/app/excel-import/excel-import.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import { Button, Card, Descriptions, Divider, Form, message, UploadProps } from 'antd';
-import { OrdersService } from '@project-management-system/shared-services';
 import Papa from 'papaparse'
 import AlertMessages from '../common/common-functions/alert-messages';
 import { useNavigate } from 'react-router-dom';
 import moment from 'moment';
 import { UndoOutlined } from '@ant-design/icons';
 import { FileStatusReq } from '@project-management-system/shared-models';
+import { OrdersService } from '@project-management-system/shared-services';
 
 
 export default function ExcelImport() {
