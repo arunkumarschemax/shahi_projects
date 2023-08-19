a73ca63a45ee7810b8b25940a276210b4ec2039a
manisha1712 <manisha.pilla@schemaxtech.com>
2023-08-19 14:56:11 +0530

    done

--- gtstoinfor/packages/ui/src/app/components/document-management/upload-view.tsx
+++ gtstoinfor/packages/ui/src/app/components/document-management/upload-view.tsx
@@ -3,7 +3,7 @@ import { Alert, Button, Card, Col, Descriptions, Divider, Form, Input, message, 
 import { PDFDocument } from 'pdf-lib';
 import Papa from 'papaparse'
 import { OrdersService, UploadDocumentService } from '@project-management-system/shared-services';
-import { ArrowDownOutlined, DownloadOutlined, UndoOutlined, UploadOutlined } from '@ant-design/icons';
+import { ArrowDownOutlined, DeleteOutlined, DownloadOutlined, UndoOutlined, UploadOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
 import { ColumnProps } from 'antd/lib/table';
 import { ColumnsType } from 'antd/es/table';
@@ -11,13 +11,13 @@ import { useForm } from 'antd/es/form/Form';
 import { AlertMessages, DocumentsListRequest, FileStatusReq, UploadDocumentListDto } from '@project-management-system/shared-models';
 
 
-const {Title, Text } = Typography;
+const { Title, Text } = Typography;
 
 export interface UploadViewProps {
-  form: FormInstance<any>;
-  docData: any;
-  formData: (value: any, filesList: any[]) => void;
-  fileList: (value: any[]) => void;
+    form: FormInstance<any>
+    docData: any;
+    formData: (value: any, filesList:any[]) => void;
+    fileList: (value: any[]) => void;
 }
 
 const UploadView = (props: UploadViewProps) => {
@@ -29,9 +29,37 @@ const UploadView = (props: UploadViewProps) => {
   const [btndisable, setBtnDisable] = useState<boolean>(true);
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
  
-//   useEffect(() =>{
-//     setLoading(false);
-// },[props.docData])
+const handleRemoveFile = (fileToRemove) => {
+  const updatedFileList = fileList.filter(file => file.uid !== fileToRemove.uid);
+  setFileList(updatedFileList);
+};
+  
+const renderFilePreviews = () => {
+  return fileList.map(file => (
+      <div key={file.uid}>
+          <p>{file.name}</p>
+          <Button onClick={() => handleRemoveFile(file)}><DeleteOutlined /></Button>
+          <Button onClick={() => handleFileDownload(file)} icon={<DownloadOutlined />}></Button>
+      </div>
+  ));
+};
+const CustomUploadList = ({ fileList, handleRemoveFile }) => {
+  console.log(fileList)
+  return (
+      <div>
+          <h3>Uploaded Files:</h3>
+          
+          <ul>
+          {fileList.length > 0 ? (
+                   renderFilePreviews()
+                ) : (
+                    <p>No files uploaded yet.</p>
+                )}
+          </ul>
+      </div>
+  );
+};
+
 
 useEffect(() =>{
   setFileList(props.docData.documentsPath);
@@ -103,47 +131,65 @@ useEffect(() =>{
     setSelectedFiles(updatedSelectedFiles);
   };
 
- 
+  const handleFileDownload = (file) => {
+    // Replace 'your-download-endpoint' with the actual URL or API endpoint to download the file.
+    const downloadUrl = `http://165.22.220.143/document-management/gtstoinfor/dist/packages/services/document-management/upload-files/PO-${props.docData.customerPo}/${file.name}`;
 
+    // Create a link element to trigger the download
+    const link = document.createElement('a');
+    link.href = downloadUrl;
+    link.target = '_blank'; // Open the download link in a new tab/window
+    link.download = file.name; // Specify the file name
+
+    // Trigger the download
+    document.body.appendChild(link);
+    link.click();
+
+    // Clean up the link element
+    document.body.removeChild(link);
+
+    // Display a success message
+    message.success(`Downloading ${file.name}`);
+};
   const gstUploadFieldProps: UploadProps = {
     multiple: true,
-    onRemove: handleFileRemove =>{
-      setFileList([]);
-    } ,
-    beforeUpload: (file: File) => {
-      if (!file.name.match(/\.(pdf|jpg|jpeg|png)$/)) {
-        message.error('Only pdf and image files are allowed!');
-        return true;
-      }
-      // var reader = new FileReader();
-      // reader.readAsArrayBuffer(file);
-      // reader.readAsArrayBuffer(file);
-      // reader.onload = data => {  
-      //         setFileList([...fileList, file]);
-      //         console.log("*****************************")
-      //         console.log(fileList)
-      //         console.log([...fileList, file])
+    onRemove: (file: any) => {
+        setFileList([]);
+        // uploadFileList([]);
+    },
+    onDownload: handleFileDownload,
+    beforeUpload: (file: any) => {
+        if (!file.name.match(/\.(pdf|jpg|jpeg|png)$/)) {
+            message.error("Only pdf and image files are allowed!");
+            return true;
+        }
+        var reader = new FileReader();
+        reader.readAsArrayBuffer(file);
+        reader.onload = data => {  
+                setFileList([...fileList, file]);
+                console.log("*****************************")
+                console.log(fileList)
+                console.log([...fileList, file])
 
 
-      //         setBtnDisable(false)
-      //         // uploadFileList([...filelist, file]);
-      //         return false;
-      handleFileUpload(file);
-      return false;
+                setBtnDisable(false)
+                // uploadFileList([...filelist, file]);
+                return false;
+        };
+        return false;
     },
     progress: {
-      strokeColor: {
-          '0%': '#108ee9',
-          '100%': '#87d068',
-      },
-      strokeWidth: 3,
-      format: (percent: any) => `${parseFloat(percent.toFixed(2))}%`,
-  },
-  
-  fileList: fileList
+        strokeColor: {
+            '0%': '#108ee9',
+            '100%': '#87d068',
+        },
+        strokeWidth: 3,
+        format: (percent: any) => `${parseFloat(percent.toFixed(2))}%`,
+    },
+    
+    // fileList: fileList
 
 };
-
   const upload = (data: any) => {
     let file = props.form.getFieldValue(`${data.documentsListId}`);
     data.file = file;
@@ -181,6 +227,7 @@ useEffect(() =>{
         <br />
         <Text strong style={{ fontSize: '18px', color: '#333', marginBottom: '10px' }}>
           <Form.Item name={props.docData.documentsListId}>
+          <CustomUploadList fileList={fileList} handleRemoveFile={handleRemoveFile} />
             <Upload
               key={props.docData.documentsListId}
               name={`uploadFile${props.docData.documentsListId}`}
@@ -189,12 +236,10 @@ useEffect(() =>{
               onChange={(info) => handleUpload(props.docData.documentsListId, info)}
 
             >
-              <Button
-           
+              <Button           
                 key={props.docData.documentsListId}
                 style={{ color: 'black', backgroundColor: '#7ec1ff' }}
                 icon={<UploadOutlined />}
-              
               >
                 Choose File
               </Button>
