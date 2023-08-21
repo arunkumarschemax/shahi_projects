ad0ff1761c204b12cfb5748cf5ac4bdc8eaa044b
manisha1712 <manisha.pilla@schemaxtech.com>
2023-08-21 15:51:02 +0530

    live connsctions

--- gtstoinfor/packages/services/document-management/config.ts
+++ gtstoinfor/packages/services/document-management/config.ts
@@ -6,11 +6,11 @@ export const appConfig = {
     },
     database: {
         type: process.env[`APP_DB_TYPE`] || 'mysql',
-        host: process.env[`APP_DB_HOST`] || '165.22.220.143',
+        host: process.env[`APP_DB_HOST`] || 'localhost',
         port: parseInt(process.env[`APP_DB_PORT`]) || 3306,
-        username: process.env[`APP_DB_USER`] || 'ramakrishna',
-        password: process.env[`APP_DB_PASS`] || 'Schemax@23',
-        dbName: process.env[`APP_DB_DBNAME`] || 'shahi_docs',
+        username: process.env[`APP_DB_USER`] || 'doc7',
+        password: process.env[`APP_DB_PASS`] || 'r2dbXahzK8ECcDWW',
+        dbName: process.env[`APP_DB_DBNAME`] || 'doc7',
         poolLimit: parseInt(process.env[`APP_DB_POOL_LIMIT`]) || 50
     },
 
