13c52f39cad186850cd040573652610d5d4eb677
saiprakashkatiki <saiprakash@schemaxtech.com>
2023-08-29 18:08:57 +0530

    login bug fix

--- gtstoinfor/packages/ui/src/app/app.tsx
+++ gtstoinfor/packages/ui/src/app/app.tsx
@@ -1,4 +1,4 @@
-  
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { useState } from 'react';
 import './app.module.css';
@@ -7,36 +7,37 @@ import { Route, Link } from 'react-router-dom';
 import axios from 'axios';
 import { AppRoutes } from './routes';
 import { LoginComponent, useIAMClientState } from './nike/iam-client-react';
-  
+import Login from './layout/login/login';
+
 
 export function App() {
   const [load, setLoad] = useState<any>();
-  const {IAMClientAuthContext,dispatch}=useIAMClientState();
+  const { IAMClientAuthContext, dispatch } = useIAMClientState();
 
 
   axios.interceptors.request.use(request => {
-      setLoad(true);
-      return request;
+    setLoad(true);
+    return request;
   });
 
   axios.interceptors.response.use(response => {
-      setLoad(false);
-      return response;
+    setLoad(false);
+    return response;
   }, error => {
-      setLoad(false);
-      throw error;
+    setLoad(false);
+    throw error;
   });
 
   return (
-    IAMClientAuthContext.isAuthenticated?
-    <>
-      <CustomSpinner loading={load} />
-      <AppRoutes />
-    </>:
-     <div >
-     <LoginComponent />
-   </div>)
-   
+    IAMClientAuthContext.isAuthenticated ?
+      <>
+        <CustomSpinner loading={load} />
+        <AppRoutes />
+      </> :
+      <div >
+        <Login />
+      </div>)
+
 
 }
 
