dcaee1e96f6483279c58d2d3a911937943156441
saiprakashkatiki <saiprakash@schemaxtech.com>
2023-09-04 13:24:33 +0530

    config file

--- gtstoinfor/packages/libs/shared-services/config.ts
+++ gtstoinfor/packages/libs/shared-services/config.ts
@@ -5,6 +5,6 @@ export const config = {
     whatsapp_broadcast_url: 'http://206.189.138.212:3232/api',
 
     // APP_PROJECT_MANAGEMENT_URL: "http://localhost:8001/api"
-    APP_IAM_SERVER_URL: "http://165.22.220.143:5000",
+    APP_IAM_SERVER_URL: "https://ums-backend.shahiapps.in",
     APP_IAM_CLIENT_ID: "225ba80a-3678-4a37-891e-4a55079df157",
 }
