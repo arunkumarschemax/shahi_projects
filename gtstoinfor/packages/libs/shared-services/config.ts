c39896dba4f411e59e7c9ed9c0a2ca27f7239228
manisha1712 <manisha.pilla@schemaxtech.com>
2023-09-07 00:40:59 +0530

    done

--- gtstoinfor/packages/libs/shared-services/config.ts
+++ gtstoinfor/packages/libs/shared-services/config.ts
@@ -6,6 +6,6 @@ export const config = {
     whatsapp_notification_url: 'http://206.189.138.212:3000',
     whatsapp_broadcast_url: 'http://206.189.138.212:3232/api',
     doc_management_url:'https://edoc7api.shahi.co.in/api',
-    // doc_management_url:'http://localhost:8002/api',
-    download_path: 'http://edoc7.shahi.co.in/document-management/gtstoinfor/dist/packages/services/document-management/'
+    // doc_management_url:'http://localhost:3002/api',
+    download_path:'https://edoc7api.shahi.co.in/',
 }
--- gtstoinfor/packages/ui/src/app/components/document-management/merge-pdf.tsx
+++ gtstoinfor/packages/ui/src/app/components/document-management/merge-pdf.tsx
@@ -27,7 +27,7 @@ const PdfMergeDownload = () => {
   const mergeAndDownloadPDFs = async () => {
     try {
       // Load the initial PDF file (you need to provide a valid URL)
-      const initialPdfUrl = 'http://localhost:8002/PO-428278-6555/sample-c2dc.pdf';
+      const initialPdfUrl = 'http://localhost:3002/PO-406170-6555/sample-7748.pdf';
 
       const initialPdfResponse = await axios.request({
         url: initialPdfUrl,
@@ -42,7 +42,7 @@ const PdfMergeDownload = () => {
 
       // Load additional PDFs from URLs (you need to provide valid PDF URLs)
       const pdfUrls = [
-        'http://localhost:8002/PO-428278-6555/sample-c2d.pdf',
+        'http://localhost:3002/PO-406170-6555/sample-7748.pdf',
       ];
       // const pdfBytesArray = await Promise.all(pdfUrls.map(async (url) => {
       //   const response = await fetch(url, { mode: 'no-cors' });
