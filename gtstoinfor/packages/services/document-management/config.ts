e03195bdd345ebc5ba12cea90a27bba551645537
Rahulsingarapu <rahul.s@schemaxtech.com>
2023-09-12 11:09:33 +0530

    database_connection

--- gtstoinfor/packages/services/document-management/config.ts
+++ gtstoinfor/packages/services/document-management/config.ts
@@ -6,11 +6,11 @@ export const appConfig = {
     },
     database: {
         type: process.env[`APP_DB_TYPE`] || 'mysql',
-        host: process.env[`APP_DB_HOST`] || 'localhost',
+        host: process.env[`APP_DB_HOST`] || '172.20.50.169',
         port: parseInt(process.env[`APP_DB_PORT`]) || 3306,
-        username: process.env[`APP_DB_USER`] || 'doc7',
+        username: process.env[`APP_DB_USER`] || 'edoc7',
         password: process.env[`APP_DB_PASS`] || 'r2dbXahzK8ECcDWW',
-        dbName: process.env[`APP_DB_DBNAME`] || 'doc7',
+        dbName: process.env[`APP_DB_DBNAME`] || 'edoc7',
         poolLimit: parseInt(process.env[`APP_DB_POOL_LIMIT`]) || 50
     },
 
