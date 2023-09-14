d9f1b8241f8ba1dbb9d229809d12818542a0ae29
Deepika <deepikapathi532@gmail.com>
2023-09-14 16:18:37 +0530

    component

---
+++ gtstoinfor/packages/ui/src/app/common/reports/ware-house-report.tsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import { Space, Table, Tag } from 'antd';
+
+
+// const WarehouseReport=()=>{
+//     const { Column, ColumnGroup } = Table;
+
+
+// // const coloumns=()=>{
+//     <Table>
+
+// <Column title="PH" dataIndex="ph" key="ph" />
+//     <ColumnGroup title="Sep">
+//     <Column title="In PCs" dataIndex="inPcs" key="inPcs" />
+//       <Column title="In Coeff" dataIndex="inCoeff" key="inCoeff" />
+//     </ColumnGroup>
+//     </Table>
+// }
+
+// // }
+// export default WarehouseReport 
