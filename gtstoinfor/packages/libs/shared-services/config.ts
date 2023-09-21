70632c7ab9bed4d0eaccffb02d95ae96cd7bb536
Rahulsingarapu <rahul.s@schemaxtech.com>
2023-09-21 12:31:00 +0530

    changes_http

--- gtstoinfor/packages/libs/shared-services/config.ts
+++ gtstoinfor/packages/libs/shared-services/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    APP_PROJECT_MANAGEMENT_URL: "https://http://uniqlov2-backend.xpparel.com/api",
+    APP_PROJECT_MANAGEMENT_URL: "https://uniqlov2-backend.xpparel.com/api",
     //APP_PROJECT_MANAGEMENT_URL: "http://165.22.220.143:8006/api",
     whatsapp_redirection_url: 'http://dev.schemaxtech.in/#',
     whatsapp_notification_url: 'http://206.189.138.212:3000',
