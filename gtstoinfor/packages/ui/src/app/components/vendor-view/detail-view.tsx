9f8bf98ed087ffcd2ea3e81bb7be01d21b8c7803
hkalahasti@gmail.com <hkalahasti@gmail.com>
2023-09-28 15:19:02 +0530

    bugs changes

--- gtstoinfor/packages/ui/src/app/components/vendor-view/detail-view.tsx
+++ gtstoinfor/packages/ui/src/app/components/vendor-view/detail-view.tsx
@@ -224,7 +224,8 @@ const VendorBranchInfoGrid = () => {
         </Descriptions.Item>
       </Descriptions>
       <br />
-      <Card style={{ textAlign: "center" }} size="small" title={<span><ApartmentOutlined />Branches</span>} >
+      { vendorDetails.length > 0 ? (
+        <Card style={{ textAlign: "center" }} size="small" title={<span><ApartmentOutlined />Branches</span>} >
         <Table
           pagination={false}
           columns={columns}
@@ -232,6 +233,7 @@ const VendorBranchInfoGrid = () => {
           size="small"
         />
       </Card>
+     ):"" }
     </Card>
   );
 };
