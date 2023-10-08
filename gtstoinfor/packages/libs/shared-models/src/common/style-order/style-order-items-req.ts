b21dbd55f02910520ff769cf2f234863b6332db4
Bali Rajeswari <balirajeswari11@gmail.com>
2023-10-08 10:32:07 +0530

    style order req and response models creation

---
+++ gtstoinfor/packages/libs/shared-models/src/common/style-order/style-order-items-req.ts
@@ -0,0 +1,37 @@
+import { CustomerOrderStatusEnum } from "../../enum";
+
+export class StyleOrderItemsReq{
+    deliveryAddress : string;
+    orderQuantity: number;
+    color : string;
+    size : string;
+    destination : string;
+    uom : string;
+    status : CustomerOrderStatusEnum;
+    discount: number;
+    salePrice: number;
+    coPercentage: number;
+    colorId: number;
+    sizeId: number;
+    destinationId: number;
+    uomId: number;
+
+    constructor(deliveryAddress : string,orderQuantity: number,color : string,size : string,destination : string,uom : string,status : CustomerOrderStatusEnum,discount: number,salePrice: number,coPercentage: number,colorId: number,sizeId: number,destinationId: number,uomId: number){
+    this.deliveryAddress = deliveryAddress
+    this.orderQuantity = orderQuantity
+    this.color = color
+    this.size = size
+    this.destination = destination
+    this.uom = uom
+    this.status = status
+    this.discount= discount
+    this.salePrice= salePrice
+    this.coPercentage= coPercentage
+    this.colorId= colorId
+    this.sizeId= sizeId
+    this.destinationId= destinationId
+    this.uomId= uomId
+
+    }
+
+}
