e1a705e8ec203bb6e9308d288d4de00d321c92cd
Venky-2329 <118739220+Venky-2329@users.noreply.github.com>
2023-10-12 22:32:21 +0530

    bugs

--- gtstoinfor/packages/ui/src/app/masters/pricelist/price-list-history.tsx
+++ gtstoinfor/packages/ui/src/app/masters/pricelist/price-list-history.tsx
@@ -45,10 +45,9 @@ export const PriceListHistory = (props: PriceListView) => {
     getUploadedTime()
   },[])
 
-  useEffect(() => {
-    console.log(fileData,'---------filedat')
+  // useEffect(() => {
 
-  },[fileData])
+  // },[fileData])
 
   const pagination = {
     current: page,
@@ -121,7 +120,6 @@ const getUploadedTime = () => {
       req.seasonCode = form.getFieldValue("seasonCode") 
     }
     priceService.getPriceHistory(req).then(res => {
-      console.log(req,'')
       if (res.status) {
         setPriceList(res.data);
       } else {
@@ -218,9 +216,9 @@ const getUploadedTime = () => {
       render: (text, record, index) => getStartIndex() + index,
     },
       {
-          title: "Item",
+          title:<div style={{ textAlign: "center" }}>Item</div>,
           dataIndex: "item",
-          align:"center",
+          align:"left",
           sorter: (a, b) => a.item.localeCompare(b.item),
           sortDirections: ["descend", "ascend"],
           // ...getColumnSearchProps("style"),
@@ -237,13 +235,14 @@ const getUploadedTime = () => {
       {
         title: "Destination",
         dataIndex: "business",
+        width:"110px",
         align:"center",
         sorter: (a, b) => a.business.localeCompare(b.business),
         sortDirections: [ "ascend","descend"],
         // ...getColumnSearchProps("destination"),
         
       },{
-        title: "Year",
+        title: <div style={{ textAlign: "center" }}>Year</div>,
         dataIndex: "year",
         align:"right",
         sorter: (a, b) => a.year - b.year,
@@ -254,36 +253,45 @@ const getUploadedTime = () => {
       {
         title: "Season Code",
         dataIndex: "season",
+        width:"110px",
         align:"center",
-        sorter: (a, b) => a.season.localeCompare(b.season),
-        sortDirections: [ "ascend","descend"],
-        // ...getColumnSearchProps("seasonCode"),
-
-       
+        // sorter: (a, b) => a.season.localeCompare(b.season),
+        // sortDirections: [ "ascend","descend"],
+        filters: [
+          {
+            text: 'FW',
+            value: "FW",
+          },
+          {
+            text: 'SS',
+            value: "SS",
+          },
+        ],
+        onFilter: (value,record) =>{ return record.season === value}
       },
       {
         title: "Price",
         children:[
           {
-            title: "Previous",
+            title: <div style={{ textAlign: "center" }}>Previous</div>,
             dataIndex: "previous_price",
             align: "right",
             render: (text, record) => {
               return (
                 <>
-                  {record.previous_price ? `${record.currency} - ${parseFloat(record.previous_price).toLocaleString()} ` : '-'}
+                  {record.previous_price ? `${record.currency} ${parseFloat(record.previous_price).toLocaleString()} ` : '-'}
                 </>
               );
             }
           },
           {
-            title: "Latest",
+            title: <div style={{ textAlign: "center" }}>Latest</div>,
             dataIndex: "current_price",
             align: "right",
             render: (text, record) => {
               return (
                 <>
-                  {record.current_price ? `${record.currency} - ${parseFloat(record.current_price).toLocaleString()} ` : '-'}
+                  {record.current_price ? `${record.currency} ${parseFloat(record.current_price).toLocaleString()} ` : '-'}
                 </>
               );
             }
@@ -291,7 +299,7 @@ const getUploadedTime = () => {
         ],
       },
       {
-        title: "Variance",
+        title: <div style={{ textAlign: "center" }}>Variance</div>,
         dataIndex: "price_variance",
         align: "right",
         render: (text) => {
@@ -300,24 +308,24 @@ const getUploadedTime = () => {
           } else if (text > 0) {
             return (
               <span style={{ color: "green" }}>
-                {text} <ArrowUpOutlined style={{ color: "green" }} />
+                {text.toLocaleString("en-US")} <ArrowUpOutlined style={{ color: "green" }} />
               </span>
             );
           } else if (text < 0) {
             return (
               <span style={{ color: "red" }}>
-                {Math.abs(text)} <ArrowDownOutlined style={{ color: "red" }} />
+                {Math.abs(text).toLocaleString("en-US")} <ArrowDownOutlined style={{ color: "red" }} />
               </span>
             );
           } else {
-            return text;
+            return text.toLocaleString("en-US");
           }
         },
+        
       }
   ];
 
   const onChange = (pagination, filters, sorter, extra) => {
-    console.log('params', pagination, filters, sorter, extra);
   }
 
   
@@ -330,7 +338,29 @@ const getUploadedTime = () => {
     extra={<Link to='/masters/pricelist/price-list-form' >
       <span style={{color:'white'}} ><Button type={'primary'} >New</Button> </span>
       </Link>} >
-     
+        <Row gutter={10}>
+        <Col>
+          <Tooltip title={fileData[1]?.fileName} arrow={false}>
+            <Card
+              title={'Previous File Name: ' + fileData[1]?.fileName}
+              style={{ textAlign: 'left', maxWidth: 330, width: "100%", height: 41, backgroundColor: '#bfbfbf' }}
+            />
+          </Tooltip>
+        </Col>
+
+        <Col>
+          <Card title={'Previous File Date:' + fileData[1]?.createdAt} style={{ textAlign: 'left', width: 285, height: 41, backgroundColor: '#B1D5F8' }}></Card>
+        </Col>
+        <Col>
+        <Tooltip title={fileData[0]?.fileName} arrow={false}>
+          <Card title={'Latest File Name: ' + fileData[0]?.fileName} style={{ textAlign: 'left', maxWidth: 330,  width:"100%",height: 41, backgroundColor: '#B1F8E2' }}></Card>
+        </Tooltip>
+        </Col>
+        <Col>
+          <Card title={'Latest File Date: ' + fileData[0]?.createdAt} style={{ textAlign: 'left', width: 270, height: 41, backgroundColor: '#CBB1F8  ' }}></Card>
+        </Col>
+      </Row>
+      <br></br>
         <Form form={form} style={{textAlign:'center'}}  layout='vertical' onFinish={getPriceList}>
         <Row gutter={24}>
           <Col xs={24} sm={12} md={8} lg={6} xl={4}  style={{ padding: '8px' }}>
@@ -394,7 +424,7 @@ const getUploadedTime = () => {
           </Col>
         </Row>
       </Form>
-      <Descriptions>
+      {/* <Descriptions>
         <Descriptions.Item label="Uploaded File Details">
           <Descriptions column={2}>
             <Descriptions.Item label="Previous File Name">{fileData != undefined ? fileData[1]?.fileName: '-'}</Descriptions.Item>
@@ -403,14 +433,21 @@ const getUploadedTime = () => {
             <Descriptions.Item label="Latest File Date">{fileData != undefined ? fileData[0]?.createdAt: '-'}</Descriptions.Item>
           </Descriptions>
         </Descriptions.Item>
-      </Descriptions>
+      </Descriptions> */}
       <Table
       rowKey={record => record}
       columns={columns}
       dataSource={priceList}
+      scroll={{x:'max-content'}}
       className="custom-table-wrapper"
-      pagination={pagination}
-      scroll={{x:true}}
+      pagination={{
+        pageSize: 100, 
+        onChange(current, pageSize) {
+            setPage(current);
+            setPageSize(pageSize);
+        }
+    }}
+      // scroll={{x:true}}
       onChange={onChange}
       bordered />
     </Card> </>
