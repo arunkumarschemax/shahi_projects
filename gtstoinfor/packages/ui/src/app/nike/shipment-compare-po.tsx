901440faa7432bba6fde240c936240280ed1123a
AKHILANDRO <gvaakhil@gmail.com>
2023-10-18 16:39:46 +0530

    divert report changes

--- gtstoinfor/packages/ui/src/app/nike/shipment-compare-po.tsx
+++ gtstoinfor/packages/ui/src/app/nike/shipment-compare-po.tsx
@@ -788,12 +788,12 @@ const ShipmentChangesCompareGrid = () => {
         },
         {
             title: 'Change To Destination Country Name',
-            dataIndex: '', width: 80,
+            dataIndex: '', width: 90,
             // ...getColumnSearchProps('')
         },
         {
             title: 'Change from Ship To Customer Number',
-            dataIndex: '', width: 80,
+            dataIndex: '', width: 90,
             // ...getColumnSearchProps('')
         },
         {
