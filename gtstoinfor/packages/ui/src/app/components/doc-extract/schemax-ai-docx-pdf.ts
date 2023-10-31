778dba29f4998edf53fa35cf945127d8bc81ba84
Jaswanth <132431551+ReactJaswanth@users.noreply.github.com>
2023-10-31 11:14:30 +0530

    Maersk Changes

--- gtstoinfor/packages/ui/src/app/components/doc-extract/schemax-ai-docx-pdf.ts
+++ gtstoinfor/packages/ui/src/app/components/doc-extract/schemax-ai-docx-pdf.ts
@@ -1430,96 +1430,173 @@ export const extractMaersk = async (pdf) => {
     const InvoiceLines = [];
     let currentInvoice = null;
     let gstNumberExtracted = false;
-    let venName = '';
-    const vendorNameId = '1-2';
-    const invoiceDateId = '1-9';
-    const invoiceDateId2 = '1-10';
-    const invoiceNumberId = '1-254';
-    const invoiceNumberId2 = '1-224';
+
+    const invoiceDateRegex = /\d{1,2} [A-Z][a-z]{2} \d{4}/;
+    const invoiceNumberRegex = /1033\d{8}/;
     const invoiceCurrency = 'INR';
     const currentYear = new Date().getFullYear();
     const nextYear = currentYear + 1;
     const financialYear = `${currentYear}-${nextYear}`;
 
     if (extractedData && Array.isArray(extractedData)) {
+        let venName = '';
         let invoiceDate = '';
         let invoiceNumber = '';
 
         for (const line of extractedData) {
-            if (line.id >= '1-201' && line.id <= '2-300') {
-                const gstMatch = line.content.match(/[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[A-Z0-9]{1}[A-Z]{1}[A-Z0-9]{1}/g);
-                if (gstMatch && !gstNumberExtracted) {
-                    const gstNumber = gstMatch[0];
+            const gstMatch = line.content.match(/[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[A-Z0-9]{1}[A-Z]{1}[A-Z0-9]{1}/g);
+            if (gstMatch && !gstNumberExtracted) {
+                const gstNumber = gstMatch[0];
 
-                    if (gstNumber === '33AAHCA6078Q1Z3') {
-                        venName = 'Damco India Pvt. Ltd';
-                    } else {
-                        const vendorNameData = extractedData.find((item) => item.id === vendorNameId);
-                        venName = vendorNameData ? vendorNameData.content : '';
-                    }
-
-                    const invoiceDateData = extractedData.find((item) => item.id === invoiceDateId);
-                    if (invoiceDateData && invoiceDateData.content.trim() !== '') {
-                        invoiceDate = invoiceDateData.content.replace(/^INVOICE DATE:\s*/, '');
-                    } else {
-                        const invoiceDateData2 = extractedData.find((item) => item.id === invoiceDateId2);
-                        if (invoiceDateData2 && invoiceDateData2.content.trim() !== '') {
-                            invoiceDate = invoiceDateData2.content.replace(/^INVOICE DATE:\s*/, '');
-                        }
-                    }
-
-                    const invoiceNumberData = extractedData.find((item) => item.id === invoiceNumberId);
-                    if (invoiceNumberData && invoiceNumberData.content.trim() !== '') {
-                        invoiceNumber = invoiceNumberData.content.replace(/^INVOICE NUMBER:\s*/, '');
-                    } else {
-                        const invoiceNumberData2 = extractedData.find((item) => item.id === invoiceNumberId2);
-                        if (invoiceNumberData2 && invoiceNumberData2.content.trim() !== '') {
-                            invoiceNumber = invoiceNumberData2.content.replace(/^INVOICE NUMBER:\s*/, '');
-                        }
-                    }
-
-                    const invoiceAmount = structuredHSNLines.reduce((add, hsnLine) => {
-                        const amount = parseFloat(hsnLine.amount) || 0;
-                        const taxAmount = parseFloat(hsnLine.taxAmount) || 0;
-                        return add + amount + taxAmount;
-                    }, 0).toFixed(2);
-
-                    const igst = structuredHSNLines.reduce((acc, hsnLine) => {
-                        const taxAmount = parseFloat(hsnLine.taxAmount) || 0;
-                        return acc + taxAmount;
-                    }, 0).toFixed(2);
-
-                    const cgst = structuredHSNLines.reduce((add, hsnLine) => {
-                        return add + parseFloat(hsnLine.cgst) || 0;
-                    }, 0).toFixed(2);
-
-                    const sgst = structuredHSNLines.reduce((acc, hsnLine) => {
-                        return acc + parseFloat(hsnLine.sgst) || 0;
-                    }, 0).toFixed(2);
-
-                    currentInvoice = {
-                        "venName": venName,
-                        "gstNumber": gstNumber,
-                        "invoiceDate": invoiceDate,
-                        "invoiceNumber": invoiceNumber,
-                        "invoiceCurrency": invoiceCurrency,
-                        "financialYear": financialYear,
-                        "invoiceAmount": invoiceAmount,
-                        "igst": igst,
-                        "cgst": cgst,
-                        "sgst": sgst,
-                    };
-                    InvoiceLines.push(currentInvoice);
-                    gstNumberExtracted = true;
+                if (gstNumber === '29AAJCS1175L1ZU' || gstNumber === '06AAJCS1175L1Z2') {
+                    venName = 'Damco India Pvt. Ltd';
                 }
+
+                const invoiceDateData = extractedData.find((item) => item.content.match(invoiceDateRegex));
+                invoiceDate = invoiceDateData ? invoiceDateData.content : '';
+
+                const invoiceNumberData = extractedData.find((item) => item.content.match(invoiceNumberRegex));
+                invoiceNumber = invoiceNumberData ? invoiceNumberData.content : '';
+                invoiceNumber = invoiceNumber.replace(/\bTAX\s*INVOICE\b/, '').trim();
+
+                const invoiceAmount = structuredHSNLines.reduce((add, hsnLine) => {
+                    const charge = parseFloat(hsnLine.charge) || 0;
+                    const taxAmount = parseFloat(hsnLine.taxAmount) || 0;
+                    return add + charge + taxAmount;
+                }, 0).toFixed(2);
+
+                let igst = "0.00";
+                let cgst = "0.00";
+                let sgst = "0.00";
+
+                for (const hsnLine of structuredHSNLines) {
+                    if (hsnLine.taxPercentage === 18) {
+                        igst = (parseFloat(igst) + parseFloat(hsnLine.taxAmount || 0)).toFixed(2);
+                    } else if (hsnLine.taxPercentage === 9) {
+                        cgst = (parseFloat(cgst) + parseFloat(hsnLine.taxAmount || 0)).toFixed(2);
+                        sgst = (parseFloat(sgst) + parseFloat(hsnLine.taxAmount || 0)).toFixed(2);
+                    }
+                }
+
+                currentInvoice = {
+                    "venName": venName,
+                    "gstNumber": gstNumber,
+                    "invoiceDate": invoiceDate,
+                    "invoiceNumber": invoiceNumber,
+                    "invoiceCurrency": invoiceCurrency,
+                    "financialYear": financialYear,
+                    "invoiceAmount": invoiceAmount,
+                    "igst": igst,
+                    "cgst": cgst,
+                    "sgst": sgst,
+                };
+
+                InvoiceLines.push(currentInvoice);
+                gstNumberExtracted = true;
             }
         }
     }
-    console.log("ALL LINES", JSON.stringify(allLines, null, 2));
+
+    console.log("DART PDF DATA", JSON.stringify(allLines, null, 2))
+    console.log("PDF HSN DATA", JSON.stringify(structuredHSNLines, null, 2));
+    console.log("PDF INVOICE Data", JSON.stringify(InvoiceLines, null, 2));
     return {
         extractedData: InvoiceLines[0],
         extractedHsnData: structuredHSNLines
-    };
+    }
+    // const InvoiceLines = [];
+    // let currentInvoice = null;
+    // let gstNumberExtracted = false;
+    // let venName = '';
+    // const vendorNameId = '1-2';
+    // const invoiceDateId = '1-9';
+    // const invoiceDateId2 = '1-10';
+    // const invoiceNumberId = '1-254';
+    // const invoiceNumberId2 = '1-224';
+    // const invoiceCurrency = 'INR';
+    // const currentYear = new Date().getFullYear();
+    // const nextYear = currentYear + 1;
+    // const financialYear = `${currentYear}-${nextYear}`;
+
+    // if (extractedData && Array.isArray(extractedData)) {
+    //     let invoiceDate = '';
+    //     let invoiceNumber = '';
+
+    //     for (const line of extractedData) {
+    //         if (line.id >= '1-201' && line.id <= '2-300') {
+    //             const gstMatch = line.content.match(/[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[A-Z0-9]{1}[A-Z]{1}[A-Z0-9]{1}/g);
+    //             if (gstMatch && !gstNumberExtracted) {
+    //                 const gstNumber = gstMatch[0];
+
+    //                 if (gstNumber === '33AAHCA6078Q1Z3') {
+    //                     venName = 'Damco India Pvt. Ltd';
+    //                 } else {
+    //                     const vendorNameData = extractedData.find((item) => item.id === vendorNameId);
+    //                     venName = vendorNameData ? vendorNameData.content : '';
+    //                 }
+
+    //                 const invoiceDateData = extractedData.find((item) => item.id === invoiceDateId);
+    //                 if (invoiceDateData && invoiceDateData.content.trim() !== '') {
+    //                     invoiceDate = invoiceDateData.content.replace(/^INVOICE DATE:\s*/, '');
+    //                 } else {
+    //                     const invoiceDateData2 = extractedData.find((item) => item.id === invoiceDateId2);
+    //                     if (invoiceDateData2 && invoiceDateData2.content.trim() !== '') {
+    //                         invoiceDate = invoiceDateData2.content.replace(/^INVOICE DATE:\s*/, '');
+    //                     }
+    //                 }
+
+    //                 const invoiceNumberData = extractedData.find((item) => item.id === invoiceNumberId);
+    //                 if (invoiceNumberData && invoiceNumberData.content.trim() !== '') {
+    //                     invoiceNumber = invoiceNumberData.content.replace(/^INVOICE NUMBER:\s*/, '');
+    //                 } else {
+    //                     const invoiceNumberData2 = extractedData.find((item) => item.id === invoiceNumberId2);
+    //                     if (invoiceNumberData2 && invoiceNumberData2.content.trim() !== '') {
+    //                         invoiceNumber = invoiceNumberData2.content.replace(/^INVOICE NUMBER:\s*/, '');
+    //                     }
+    //                 }
+
+    //                 const invoiceAmount = structuredHSNLines.reduce((add, hsnLine) => {
+    //                     const amount = parseFloat(hsnLine.amount) || 0;
+    //                     const taxAmount = parseFloat(hsnLine.taxAmount) || 0;
+    //                     return add + amount + taxAmount;
+    //                 }, 0).toFixed(2);
+
+    //                 const igst = structuredHSNLines.reduce((acc, hsnLine) => {
+    //                     const taxAmount = parseFloat(hsnLine.taxAmount) || 0;
+    //                     return acc + taxAmount;
+    //                 }, 0).toFixed(2);
+
+    //                 const cgst = structuredHSNLines.reduce((add, hsnLine) => {
+    //                     return add + parseFloat(hsnLine.cgst) || 0;
+    //                 }, 0).toFixed(2);
+
+    //                 const sgst = structuredHSNLines.reduce((acc, hsnLine) => {
+    //                     return acc + parseFloat(hsnLine.sgst) || 0;
+    //                 }, 0).toFixed(2);
+
+    //                 currentInvoice = {
+    //                     "venName": venName,
+    //                     "gstNumber": gstNumber,
+    //                     "invoiceDate": invoiceDate,
+    //                     "invoiceNumber": invoiceNumber,
+    //                     "invoiceCurrency": invoiceCurrency,
+    //                     "financialYear": financialYear,
+    //                     "invoiceAmount": invoiceAmount,
+    //                     "igst": igst,
+    //                     "cgst": cgst,
+    //                     "sgst": sgst,
+    //                 };
+    //                 InvoiceLines.push(currentInvoice);
+    //                 gstNumberExtracted = true;
+    //             }
+    //         }
+    //     }
+    // }
+    // console.log("ALL LINES", JSON.stringify(allLines, null, 2));
+    // return {
+    //     extractedData: InvoiceLines[0],
+    //     extractedHsnData: structuredHSNLines
+    // };
 
 
 }
