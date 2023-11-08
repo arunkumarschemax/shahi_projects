f99ef16d5752d09a1e0a2b23894f2d80119aef8c
Renukaattada <renukaattata21@gmail.com>
2023-11-08 18:02:54 +0530

    #purchase order entity created

---
+++ gtstoinfor/packages/services/common/src/app/purchase-order/entities/purchase-order-entity.ts
@@ -0,0 +1,90 @@
+import { PurchaseOrderStatus } from "@project-management-system/shared-models";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, VersionColumn } from "typeorm";
+
+@Entity('purchase_order')
+export class PurchaseOrderEntity{
+    @PrimaryGeneratedColumn('increment',{
+        name:'purchase_order_id'
+    })
+    purchaseOrderId:number
+
+    @Column('varchar',{
+        name:'po_number',
+        nullable:false
+    })
+    poNumber:string
+
+    @Column('int',{
+        name:'vendor_id',
+        nullable:false
+    })
+    vendorId:number
+
+    @Column('int',{
+        name:'style_id',
+        nullable:false
+    })
+    styleId:number
+
+
+    @Column('date',{
+        name:'expected_delivery_date',
+        nullable:false
+    })
+    expectedDeliveryDate:Date
+
+    @Column('date',{
+        name:'purchase_order_date',
+        nullable:false
+    })
+    purchaseOrderDate:Date
+
+     @CreateDateColumn({
+        name: "created_at",
+      })
+      createdAt: string;
+    
+      @Column("varchar", {
+        nullable: true,
+        length: 40,
+        name: "created_user",
+      })
+      createdUser: string | null;
+    
+      @UpdateDateColumn({
+        name: "updated_at",
+      })
+      updatedAt: string;
+    
+      @Column("varchar", {
+        nullable: true,
+        length: 40,
+        name: "updated_user",
+      })
+      updatedUser: string | null;
+    
+      @VersionColumn({
+        default: 1,
+        name: "version_flag",
+      })
+      versionFlag: number;
+
+      @Column({
+        nullable: false,
+        name: "is_active",
+        default:1
+      })
+      isActive: boolean;
+
+      @Column('enum',{
+        name:'status',
+        enum:PurchaseOrderStatus
+      })
+        status:PurchaseOrderStatus
+
+    @Column('text',{
+        name:'remarks',
+        nullable:true
+    })
+    remarks:string
+}
