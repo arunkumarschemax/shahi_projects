e15e89b6b1e095ab73cf5bac8aa043125a2d8d71
Deepika <deepikapathi532@gmail.com>
2023-11-13 10:53:29 +0530

    bug fixes

--- gtstoinfor/packages/ui/src/app/masters/master-brands/master.brands-gridt.tsx
+++ gtstoinfor/packages/ui/src/app/masters/master-brands/master.brands-gridt.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import {  Divider, Table, Popconfirm, Card, Tooltip, Switch,Input,Button,Tag,Row, Col, Drawer, Space, message } from 'antd';
+import {  Divider, Table, Popconfirm, Card, Tooltip, Switch,Input,Button,Tag,Row, Col, Drawer, Space, Checkbox, message } from 'antd';
 import {CheckCircleOutlined,CloseCircleOutlined,RightSquareOutlined,EyeOutlined,EditOutlined,SearchOutlined } from '@ant-design/icons';
 import Highlighter from 'react-highlight-words';
 import { ColumnProps, ColumnType } from 'antd/lib/table';
