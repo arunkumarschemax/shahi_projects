1b0eb2369db5c1ccc2898fad9bc71dde7d5c9ded
manisha1712 <manisha.pilla@schemaxtech.com>
2023-11-14 12:47:48 +0530

    done

--- gtstoinfor/packages/services/common/src/app/app.module.ts
+++ gtstoinfor/packages/services/common/src/app/app.module.ts
@@ -75,6 +75,7 @@ import { PurchaseOrderModule } from './purchase-order/purchase-order-module';
 import { RacksModule } from './racks/rack.module';
 import { QualityModule } from './quality/quality.module';
 import { M3StyleModule } from './m3-style-codes/m3-style.module';
+import { GrnModule } from './grn/grn.module';
 
 
 @Module({
@@ -135,7 +136,7 @@ import { M3StyleModule } from './m3-style-codes/m3-style.module';
     RacksModule,
     QualityModule,
     M3StyleModule,
-    AuthModule, JwtModule,EmployeeDetailsModule,ItemsModule,VendorsModule,BuyersModule,CompanyModule,AccountControlObjectModule,OperationGroupsModule,CountriesModule,GarmentCategoriesModule,StyleModule,PaymentMethodModule ,ComponentMappingModule,ProfitControlHeadModule,CountriesModule,GarmentCategoriesModule,StyleModule,ComponentMappingModule,WarehouseModule,TaxesModule,SettingsModule,ColourModule,UomModule,DestinationModule,SampleTypesModule,SampleSubTypesModule,OperationSequenceModule,FabricWeaveModule,M3MastersModule,FabricDevelopmentModule,SkuGenerationModule,SampleDevReqModule,StyleOrderModule,OperationTrackingModule,MaterialIssueModule,IndentModule,RackPositionModule,StocksModule],
+    AuthModule, JwtModule,EmployeeDetailsModule,ItemsModule,VendorsModule,BuyersModule,CompanyModule,AccountControlObjectModule,OperationGroupsModule,CountriesModule,GarmentCategoriesModule,StyleModule,PaymentMethodModule ,ComponentMappingModule,ProfitControlHeadModule,CountriesModule,GarmentCategoriesModule,StyleModule,ComponentMappingModule,WarehouseModule,TaxesModule,SettingsModule,ColourModule,UomModule,DestinationModule,SampleTypesModule,SampleSubTypesModule,OperationSequenceModule,FabricWeaveModule,M3MastersModule,FabricDevelopmentModule,SkuGenerationModule,SampleDevReqModule,StyleOrderModule,OperationTrackingModule,MaterialIssueModule,IndentModule,RackPositionModule,StocksModule,GrnModule],
   controllers: [AppController],
   providers: [AppService],
 })
