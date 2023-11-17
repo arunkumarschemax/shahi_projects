f132cb728b0693f86de1056efea356e422bf5580
saiprakashkatiki <saiprakash@schemaxtech.com>
2023-11-17 11:06:47 +0530

    Cron job

--- gtstoinfor/packages/services/common/src/app/dpom/nike-dpom.service.ts
+++ gtstoinfor/packages/services/common/src/app/dpom/nike-dpom.service.ts
@@ -47,7 +47,7 @@ export class DpomService {
     ) { }
 
     async getOctaToken() {
-        const payload = { 'grant_type': 'password', 'scope': 'iam.okta.factoryaffiliations.read iam.okta.factorygroups.read openid legacy_username email', 'username': 'aranganathan.muthukrishnan@shahi.co.in', 'password': 'Nike@123' }
+        const payload = { 'grant_type': 'password', 'scope': 'iam.okta.factoryaffiliations.read iam.okta.factorygroups.read openid legacy_username email', 'username': 'aranganathan.muthukrishnan@shahi.co.in', 'password': 'Nike@12345' }
         const headers = {
             'Authorization': 'Basic TklLRS5HU00uREgtQVBJOnVyNjNiZjR1cEIya1FKdUkxaEV6bEdYa3Z5Rjg1WHNFVE0zR0lZY3ROSDVYeVM1YU9KVDJpNVNkaWNyTUk1Nk0=', 'Cookie': 'JSESSIONID=09FD9CE633210E9561E3E8583203D2CD', 'Cache-Control': 'no-cache', 'Content-Type': 'application/x-www-form-urlencoded', 'Accept': '*/*', 'Connection': 'keep-alive', 'Accept-Encoding': 'gzip, deflate, br'
         }
@@ -541,7 +541,7 @@ export class DpomService {
     //     }
     // }
 
-    @Cron('0 2 * * *')
+    @Cron('0 21 * * *')
     async saveDPOMApiDataToDataBase(): Promise<CommonResponseModel> {
         const transactionManager = new GenericTransactionManager(this.dataSource)
         try {
@@ -672,6 +672,7 @@ export class DpomService {
         }
     }
 
+    @Cron('0 2 * * *')
     async syncCRMData(): Promise<CommonResponseModel> {
         // const transactionManager = new GenericTransactionManager(this.dataSource)
         const getBuyerPOs = await this.dpomRepository.getBuyerPOs()
@@ -2170,7 +2171,7 @@ export class DpomService {
             return new CommonResponseModel(false, 0, error)
         }
     }
-    
+
 
     async getPpmDocTypeForFactory(): Promise<CommonResponseModel> {
         const data = await this.dpomRepository.getPpmDocTypeForFactory()
@@ -2207,7 +2208,7 @@ export class DpomService {
         else
             return new CommonResponseModel(false, 0, 'No data found');
     }
-    
+
     async getPpmPlanningSeasonYearFactory(): Promise<CommonResponseModel> {
         const data = await this.dpomRepository.getPpmplanningSeasonYearForFactory()
         if (data.length > 0)
