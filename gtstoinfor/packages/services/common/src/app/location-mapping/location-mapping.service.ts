05ef650183fcfe3beb19bc25858614685ce07e71
manisha1712 <manisha.pilla@schemaxtech.com>
2023-11-30 12:46:15 +0530

    done

--- gtstoinfor/packages/services/common/src/app/location-mapping/location-mapping.service.ts
+++ gtstoinfor/packages/services/common/src/app/location-mapping/location-mapping.service.ts
@@ -22,6 +22,7 @@ export class LocationMappingService {
             let dataquery = `SELECT 
             grn_it.grn_item_id,
             grn_it.grn_id,
+            grn_it.m3_item_code_id,
             grn_it.received_quantity,
             grn_it.received_uom_id,
             grn_it.accepted_quantity,
