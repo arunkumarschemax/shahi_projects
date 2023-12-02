db5bf6f3b946470be12e8e233e5882af29f2eeac
hkalahasti@gmail.com <hkalahasti@gmail.com>
2023-12-02 17:38:23 +0530

    variety and trim backend added

--- gtstoinfor/packages/services/common/src/app/Trim Masters/trim/trim-services.ts
+++ gtstoinfor/packages/services/common/src/app/Trim Masters/trim/trim-services.ts
@@ -5,7 +5,7 @@ import { Console } from 'console';
 import { ErrorResponse } from 'packages/libs/backend-utils/src/models/global-res-object';
 import { trimEntity } from './trim-entity';
 import { TrimAdapter } from './dto/trim-adapter';
-import { AllTrimResponseModel } from '@project-management-system/shared-models';
+import { AllTrimResponseModel, CommonResponseModel } from '@project-management-system/shared-models';
 import { TrimDTO } from './dto/trim-dto';
 
 @Injectable()
@@ -16,6 +16,45 @@ export class trimService{
         private trimRepository: Repository<trimEntity>,
         private trimAdapter: TrimAdapter,
       ){}
+
+      async createTrim(DTO: TrimDTO, isUpdate: boolean): Promise<CommonResponseModel> {
+        console.log(DTO,'_____________________')
+        try {
+          if (!isUpdate) {
+            const existingDept = await this.trimRepository.findOne({ where: { trimCategory: DTO.trimCategory }});
+            const existingDept1 = await this.trimRepository.findOne({ where: { trimCategory: DTO.trimCategory }});
+
+            if (existingDept) {
+              throw new CommonResponseModel(false, 11104, 'Trim Category already exists');
+            } 
+          } else {
+            const REC = await this.trimRepository.findOne({ where: { trimCategory: DTO.trimCategory } });
+            const Id = await this.trimRepository.findOne({ where: { trimId: DTO.trimId } });
+            
+            if (REC && Id.trimId !== DTO.trimId) {
+              throw new CommonResponseModel(false, 11104, 'Trim Category already exists');
+            }
+            
+            if (!DTO || !Id) {
+              throw new ErrorResponse(11104, 'Given Trim Category does not exist');
+            }
+          }
+          
+          const convVertiety: trimEntity = this.trimAdapter.convertDtoToEntity(DTO, isUpdate);
+          const savedVarEntity: trimEntity = await this.trimRepository.save(convVertiety);
+          const savedDTO: TrimDTO = this.trimAdapter.convertEntityToDto(savedVarEntity);
+          
+          if (savedDTO) {
+            const response = new CommonResponseModel(true, 1, isUpdate ? 'Trim Category Updated Successfully' : 'Trim Category Created Successfully');
+            return response;
+          } else {
+            throw new CommonResponseModel(false, 11106, 'Trim Category saved but issue while transforming into DTO');
+          }
+        } catch (error) {
+          return error;
+        }
+      }
+      
 
         async getAllTrim(): Promise<AllTrimResponseModel> {
     
@@ -65,7 +104,82 @@ export class trimService{
         } catch (err) {
           return err;
         }
-      }  
+      }
+
+      async getTrimById(trimId: number): Promise<trimEntity> {
+        const Response = await this.trimRepository.findOne({
+        where: {trimId: trimId},
+        });
+        // console.log(employeeResponse);
+        if (Response) {
+        return Response;
+        } else {
+        return null;
+        }
+    } 
+
+  async activateOrDeactivateTrim(req: TrimDTO): Promise<AllTrimResponseModel> {
+    try {
+        const Exist = await this.getTrimById(req.trimId);
+        if (Exist) {
+            if (!Exist) {
+                throw new AllTrimResponseModel(false,10113, 'Someone updated the current   Trim category information.Refresh and try again');
+            } else {
+                
+                    const Status =  await this.trimRepository.update(
+                        { trimId: req.trimId },
+                        { isActive: req.isActive,updatedUser: req.updatedUser });
+                   
+                    if (Exist.isActive) {
+                        if (Status.affected) {
+                            const Response: AllTrimResponseModel = new AllTrimResponseModel(true, 10115, 'Trim Category is Deactivated successfully');
+                            return Response;
+                        } else {
+                            throw new AllTrimResponseModel(false,10111, 'Trim Category is already deactivated');
+                        }
+                    } else {
+                        if (Status.affected) {
+                            const response: AllTrimResponseModel = new AllTrimResponseModel(true, 10114, 'Trim Category is Activated successfully');
+                            return response;
+                        } else {
+                            throw new AllTrimResponseModel(false,10112, 'Trim Category is already  activated');
+                        }
+                    }
+                // }
+            }
+        } else {
+            throw new AllTrimResponseModel(false,99998, 'No Records Found');
+        }
+    } catch (err) {
+        return err;
+    }
+}
+    
+
+async getActiveTrimById(req: TrimDTO): Promise<AllTrimResponseModel> {
+  console.log(req,"serv")
+  try {
+      const Entity: trimEntity = await this.trimRepository.findOne({
+        where:{trimId:req.trimId}
+        });
+        const data: TrimDTO = this.trimAdapter.convertEntityToDto(Entity);
+        if (data) {
+            const response = new AllTrimResponseModel(true, 11101 , 'Trim Category retrived Successfully',[data]);
+            return response;
+        }
+        else{
+            throw new AllTrimResponseModel(false,11106,'Something went wrong');
+        }
+  } catch (err) {
+      return err;
+  }
+}
+
+      
+
+
+
+
 
 
     
--- gtstoinfor/packages/services/common/src/app/Trim Masters/variety/variety-controller.ts
+++ gtstoinfor/packages/services/common/src/app/Trim Masters/variety/variety-controller.ts
@@ -1,8 +1,9 @@
 import { ApplicationExceptionHandler } from '@project-management-system/backend-utils';
 import { Body, Controller, Post, Req } from '@nestjs/common';
 import { ApiBody, ApiTags } from '@nestjs/swagger';
-import { varietyService } from './dto/variety-services';
-import { AllVarietysResponseModel } from '@project-management-system/shared-models';
+import { varietyService } from './variety-services';
+import { AllVarietysResponseModel, CommonResponseModel } from '@project-management-system/shared-models';
+import { VarietyDTO } from './dto/variety-dto';
 
 @ApiTags('variety')
 @Controller('variety')
@@ -12,6 +13,26 @@ export class VarietyController {
         private readonly applicationExceptionhandler: ApplicationExceptionHandler,
 
       ){}
+      @Post('/createVariety')
+    @ApiBody({type: VarietyDTO})
+    async createVariety(@Body() VarietyDTO:any,isUpdate:boolean=false): Promise<CommonResponseModel> {
+    try {
+        return await this.varietyservice.createVariety(VarietyDTO, false);
+    } catch (error) {
+        return this.applicationExceptionhandler.returnException(CommonResponseModel, error);
+    }
+    }
+
+    @Post('/updateVariety')
+    @ApiBody({type:VarietyDTO})
+    async updateVariety(@Body() VarietyDTO: any): Promise<CommonResponseModel> {
+        try {
+        return await this.varietyservice.createVariety(VarietyDTO, true);
+        } catch (error) {
+            return this.applicationExceptionhandler.returnException(CommonResponseModel, error)
+        }
+    }
+
 
       @Post('/getAllActiveVariety')
       async getAllActiveVariety(@Req() request: Request): Promise<AllVarietysResponseModel> {
@@ -29,6 +50,29 @@ export class VarietyController {
           return this.applicationExceptionhandler.returnException(AllVarietysResponseModel, error);
         }
       } 
+  
+      @Post('/activateOrDeactivateVariety')
+      @ApiBody({type:VarietyDTO})
+      async activateOrDeactivateVariety(@Body() Req: any): Promise<CommonResponseModel> {
+        console.log(Req,"req comnn")
+          try {
+              return await this.varietyservice.activateOrDeactivateVariety(Req);
+          } catch (err) {
+              return this.applicationExceptionhandler.returnException(CommonResponseModel, err);
+          }
+      }
+
+      @Post('/getActiveVarietyById')
+      @ApiBody({type:VarietyDTO})
+      async getActiveVarietyById(@Body()req:any): Promise<AllVarietysResponseModel> {
+        console.log(req,"coo")
+        try {
+          return await this.varietyservice.getActiveVarietyById(req);
+        } catch (error) {
+          return this.applicationExceptionhandler.returnException(AllVarietysResponseModel, error);
+        }
+        
+    }
   
    
 
