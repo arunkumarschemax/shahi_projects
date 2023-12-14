f66bf3076bc4398f89086d31cbf1df3b44840e55
Venky-2329 <118739220+Venky-2329@users.noreply.github.com>
2023-12-14 10:59:53 +0530

    grn item code

--- gtstoinfor/packages/services/common/src/app/grn/grn.service.ts
+++ gtstoinfor/packages/services/common/src/app/grn/grn.service.ts
@@ -306,24 +306,18 @@ export class GrnService {
             // console.log(req,'--------------')
             let query = `SELECT g.grn_number AS grnNumber,gi.received_quantity AS receivedQty,gi.accepted_quantity AS acceptedQty,gi.rejected_quantity  AS rejectedQty,u.uom,
             gi.conversion_quantity  AS conversionQty,uom.uom AS convertedUom,gi.location_mapped_status AS locMapStatus,gi.remarks,gi.m3_item_code_id AS m3ItemCodeId,m3.item_code AS itemCode,gi.buyer_id AS buyerId, CONCAT(b.buyer_code,'-',b.buyer_name) AS buyerName
-            FROM grn_items gi`
-            // if (req.itemType === 'FABRIC' || req.itemType === 'Fabric') {
-            //     query = query + `gi.m3_item_code_id AS m3ItemCodeId,m3.item_code AS itemCode
-            //     FROM grn_items gi
-            //     LEFT JOIN m3_items m3 ON m3.m3_items_id = gi.m3_item_code_id`
-            // }
-            // if (req.itemType.includes('TRIM') || req.itemType.includes('Trim')) {
-            //     query = query + `m3.trim_code AS itemCode,m3.m3_items_id AS m3ItemId
-            //     FROM grn_items gi
-            //     LEFT JOIN m3_items m3 ON m3.m3_items_id = gi.m3_item_code_id`
-            // }
-            query = query + ` LEFT JOIN grn g ON g.grn_id = gi.grn_id
-            LEFT JOIN m3_items m3 ON m3.m3_items_id = gi.m3_item_code_id
+            FROM grn_items gi LEFT JOIN grn g ON g.grn_id = gi.grn_id
             LEFT JOIN purchase_order po ON po.purchase_order_id = g.po_id
             LEFT JOIN vendors v ON v.vendor_id = g.vendor_id
             LEFT JOIN uom u ON u.id = gi.uom_id
             LEFT JOIN uom uom ON uom.id = gi.conversion_uom_id
-            LEFT JOIN buyers b ON b.buyer_id = gi.buyer_id`
+            LEFT JOIN buyers b ON b.buyer_id = gi.buyer_id `
+            if (req.itemType === 'Fabric') {
+                query = query + ` LEFT JOIN m3_items m3 ON m3.m3_items_id = gi.m3_item_code_id`
+            }
+            if (req.itemType !== 'Fabric') {
+                query = query + ` LEFT JOIN m3_trims m3 ON m3.m3_trim_id = gi.m3_item_code_id`
+            }
             if (req?.grnId) {
                 query = query + ` where g.grn_id=${req.grnId}`
             }
