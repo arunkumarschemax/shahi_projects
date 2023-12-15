1774e6e57d66cb92831253cc5a91673fbc2fe10f
Venky-2329 <118739220+Venky-2329@users.noreply.github.com>
2023-12-15 10:19:13 +0530

    po detail view

--- gtstoinfor/packages/services/common/src/app/purchase-order/purchase-order-service.ts
+++ gtstoinfor/packages/services/common/src/app/purchase-order/purchase-order-service.ts
@@ -487,17 +487,22 @@ export class PurchaseOrderService {
         let columnName
         // let poData = `select * from purchase_order po `
         if(poTypeRes[0].po_material_type == 'Fabric'){
-            concatString = ` left join m3_items mi on mi.m3_items_Id = poi.m3_item_id`
+            concatString = ` 
+            left join m3_items mi on mi.m3_items_Id = poi.m3_item_id 
+            LEFT JOIN indent_fabric ii ON ii.ifabric_id = poi.indent_item_id`
             columnName = 'mi.item_code'
         //     poData = poData+` left join purchase_order_fabric pof on pof.purchase_order_id = po.purchase_order_id `
         }else{
-            concatString = ` left join m3_trims mi on mi.m3_trim_Id = poi.m3_item_id`
+            concatString = ` 
+            left join m3_trims mi on mi.m3_trim_Id = poi.m3_item_id
+            LEFT JOIN indent_trims ii ON ii.itrims_id = poi.indent_item_id`
             columnName = 'mi.trim_code as item_code'
         //     poData = poData+` left join purchase_order_trim pot on pot.purchase_order_id = po.purchase_order_id`
         }
         // poData = poData+` where po.purchase_order_id = ${req.id}`
         // const podatares = await this.dataSource.query(poData)
-        const poTrimData = `select po.*,poi.*,${columnName},v.vendor_name,v.contact_number,v.bank_acc_no,v.gst_number,v.postal_code,f.address from purchase_order po left join purchae_order_items poi on poi.purchase_order_id = po.purchase_order_id ${concatString} left join factory f on f.id = po.delivery_address left join vendors v on v.vendor_id = po.vendor_id where po.purchase_order_id = ${req.id} `
+        const poTrimData = `select po.*,poi.*,${columnName},i.request_no as indentNo,i.indent_date as indentDate,v.vendor_name,v.contact_number,v.bank_acc_no,v.gst_number,v.postal_code,f.address,t.tax_percentage AS taxPercentage from purchase_order po left join purchae_order_items poi on poi.purchase_order_id = po.purchase_order_id ${concatString} LEFT JOIN indent i ON i.indent_id = ii.indent_id left join factory f on f.id = po.delivery_address left join vendors v on v.vendor_id = po.vendor_id LEFT JOIN taxes t ON t.tax_id = poi.tax
+        where po.purchase_order_id = ${req.id} `
         console.log(poTrimData,'ppppppphhh')
         const poTrimdatares = await this.dataSource.query(poTrimData)
         // console.log(podatares)
