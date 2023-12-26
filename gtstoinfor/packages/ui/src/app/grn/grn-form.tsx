fa1b17379a9e8bf2b71ff9e0e280bf0180b924e4
manisha1712 <manisha.pilla@schemaxtech.com>
2023-12-26 22:56:10 +0530

    grn duplicate grn no

--- gtstoinfor/packages/ui/src/app/grn/grn-form.tsx
+++ gtstoinfor/packages/ui/src/app/grn/grn-form.tsx
@@ -70,7 +70,7 @@ const GRNForm = () => {
 
       const values = form.getFieldsValue()
       console.log(values,'rrrr')
-      const req = new GrnDto(values.vendorId, values.purchaseOrderId, form.getFieldValue('grnDate').format('YYYY-MM-DD'), PurchaseOrderStatus.OPEN, values.remarks, undefined, undefined, '', undefined, '', 0, 0, poData?.poMaterialType, poItemData, 0, '',values.grnType, values.invoiceNo, poData?.poMaterialType, values.grnAmount,form.getFieldValue('invoiceDate').format('YYYY-MM-DD'));
+      const req = new GrnDto(values.vendorId, values.purchaseOrderId, form.getFieldValue('grnDate').format('YYYY-MM-DD'), PurchaseOrderStatus.OPEN, values.remarks, undefined, undefined, '', undefined, '', 0, 0, poData?.poMaterialType, poItemData, 0, '',values.grnType, values.invoiceNo, ((poData?.poMaterialType).toUpperCase() === "FABRIC"?(poData?.poMaterialType).toUpperCase():"Trim"), values.grnAmount,form.getFieldValue('invoiceDate').format('YYYY-MM-DD'));
       console.log(req);
       grnService.createGrn(req).then((res) => {
         if (res.status) {
