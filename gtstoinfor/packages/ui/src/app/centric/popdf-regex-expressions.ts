49795710a3f21432acab36d6d5c89296bfcaf5a2
Jaswanth <132431551+ReactJaswanth@users.noreply.github.com>
2023-12-27 17:30:26 +0530

    extraction changes for 2 formats

--- gtstoinfor/packages/ui/src/app/centric/popdf-regex-expressions.ts
+++ gtstoinfor/packages/ui/src/app/centric/popdf-regex-expressions.ts
@@ -1,3 +1,5 @@
+import exp from "constants"
+ 
 export const EMP_STR_EXP = /^\s*$/
 export const ITEM_NO_EXP = "PO Line"
 export const ITEM_NO_INDEX = 0
@@ -15,42 +17,50 @@ export const ITEM_MATERIAL_INDEX = 'PO Line'
 // export const ITEM_BILL_TO_END_TEXT = 'Agent/IMO'
 // export const ITEM_BILL_TO_END_INDEX = ITEM_BILL_TO_END_TEXT // 6
 // export const ITEM_TEXT_TEXT = 'ITEM TEXT'
-export const ITEM_TEXT_END_TEXT= 'Total Eaches'
-export const ITEM_TEXT_END_TEXT2 = 'Merchandise Amount'
-export const UNWANTED_TEXT_1 = 'Centric Fashion LLC.'
+export const MANUFACTURE_1= 'Centric CAN Apparel & Acc ULC'
+export const MANUFACTURE_2='Centric Fashion LLC.'
+export const FRIEGHT_PAY_METHOD='Freight Pay Method'
+export const REFRENCE='Shipment Method'
+export const PAYMENT_TERM_DESCRIPTION='Payment Terms Description'
+export const SPECIAL_INSTRUCTIONS='Special Instructions'
+
+export const ITEM_TEXT_END_TEXT= 'Total Eaches' 
+export const ITEM_TEXT_END_TEXT2 = 'Merchandise Amount' 
+export const UNWANTED_TEXT_1 = 'Centric Fashion LLC.' 
 export const UNWANTED_TEXT_2 = "THE MERCHANDISE ORDERED HEREWITH IS SUBJECT TO THE VENDOR OBLIGATIONS SET FORTH IN THE RALPH LAUREN CORPORATION'S"
 export const UNWANTED_TEXT_3 = "VENDOR COMPLIANCE PACKET. BY ACCEPTING THIS ORDER YOU CONFER YOUR AGREEMENT TO COMPLY WITH ALL TERMS AND"
 export const UNWANTED_TEXT_4 = "CONDITIONS SET FORTH IN THE VENDOR COMPLIANCE PACKET, INCLUDING, BUT NOT LIMITED TO THE PURCHASE ORDER TERMS AND"
 export const UNWANTED_TEXT_5 = "By Purchase Agent: ___________________"
 export const UNWANTED_TEXT_6 = "4-25-15 JINGUMAE, SHIBUYA-KU, TOKYO, 150-0001, JP"
-export const UNWANTED_TEXT_7 = "CONDITIONS."
-export const UNWANTED_TEXT_8 = "Quantity"
-export const UNWANTED_TEXT_9 = "(USD)"
-export const UNWANTED_TEXT_10 = "(USD)"
-export const UNWANTED_TEXT_11 = "Date"
-export const UNWANTED_TEXT_12 = "(USD)"
+export const UNWANTED_TEXT_7 = "CONDITIONS." 
+export const UNWANTED_TEXT_8 = "Quantity" 
+export const UNWANTED_TEXT_9 = "(USD)" 
+export const UNWANTED_TEXT_10 = "(USD)" 
+export const UNWANTED_TEXT_11 = "Date"  
+export const UNWANTED_TEXT_12 = "(USD)" 
 export const UNWANTED_TEXT_13 = "RALPH LAUREN CHILDRENSWEAR"
 // export const UNWANTED_TEXT_14 = "Quantity"
 // export const UNWANTED_TEXT_15 = "(USD)"
 // export const UNWANTED_TEXT_16 = "(USD)"
 // export const UNWANTED_TEXT_17 = "Date"
 // export const UNWANTED_TEXT_18 = "(USD)"
-export const PO_DOC_DATE_TXT = 'Season'
-export const PO_DOC_DATE_INDEX = 1
+export const PO_DOC_DATE_TXT = 'Season' 
+export const PO_DOC_DATE_INDEX = 1 
 // export const SEASONYEAR_INDEX = 2
 // export const COLOR_DESC = 'Color Description'
 // export const CURR_INDEX = 4
 // export const INCOTERMS_INDEX = [5, 8]
 // export const FACTORYLOCATION_INDEX = 6
-export const PO_NUMBER_TEXT = 'Purchase Order Date'
-export const PO_NUMBER_INDEX = 1
+export const PO_NUMBER_TEXT = 'Purchase Order Date' 
+export const PO_NUMBER_INDEX = 1 
 // export const SELLER_ADDRESS_START_TEXT = "SELLER:"
 // export const SELLER_ADDRESS_END_TEXT = "BUYER:"
-export const ITEM_VARIANT_START_TEXT = "Retail Price"
+export const ITEM_VARIANT_START_TEXT = "Retail Price" 
 
 
- // Jaswanth Chnages 2nd format//
-export const ITEM_TEXT_END_TEXT1= 'Eaches'
+ // Jaswanth Chnages 2nd format// 
+export const ITEM_TEXT_END_TEXT1= 'Per Pack' 
+export const FORMAT_SEPARATION_KEYWORD = 'Compt. Material' 
 
 
 
