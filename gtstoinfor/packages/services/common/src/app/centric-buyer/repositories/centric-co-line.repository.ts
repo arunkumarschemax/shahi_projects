6012b9c468150a7a91b3d18fc78e39d9dd3fe51d
saiprakashkatiki <saiprakash@schemaxtech.com>
2024-01-01 14:31:38 +0530

    config

--- gtstoinfor/packages/services/common/src/app/centric-buyer/repositories/centric-co-line.repository.ts
+++ gtstoinfor/packages/services/common/src/app/centric-buyer/repositories/centric-co-line.repository.ts
@@ -53,8 +53,8 @@ export class CentricCOLineRepository extends Repository<CentricCOLineEntity> {
 
     async getDataforCOLineCreation(): Promise<any[]> {
         const query = this.createQueryBuilder('co')
-            .select(`co.id, co.buyer_po, co.line_item_no, co.item_no, co.buyer`)
-            .where(` status != 'Success' AND is_active = true`)
+            .select(`co.id, co.po_number, co.po_line, co.item_no, co.buyer`)
+            .where(` status != 'Success' AND status != 'Inprogress' AND is_active = true`)
             .orderBy(` created_at`, 'ASC')
         return await query.getRawMany();
     }
