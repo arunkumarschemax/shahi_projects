dcc9b0fbde8d1e8facddef6548e500f40a029919
sunanda kondala <sunandakondala@gmail.com>
2024-01-02 23:52:44 +0530

    bugs fixex

--- gtstoinfor/packages/libs/shared-models/src/common/indent/indent-request-dto.ts
+++ gtstoinfor/packages/libs/shared-models/src/common/indent/indent-request-dto.ts
@@ -4,14 +4,18 @@ export class IndentRequestDto {
      confirmEndDate?:string;
      extRefNo?:string;
     tab?:string
+    expectedStartDate?:string;
+     expectedEndDate?:string;
     constructor(requestNo?: string,    
-        confirmStartDate?:string,confirmEndDate?:string,extRefNo?:string,tab?:string) {
+        confirmStartDate?:string,confirmEndDate?:string,extRefNo?:string,tab?:string,expectedStartDate?:string,
+        expectedEndDate?:string) {
         this.requestNo= requestNo;
         this.confirmStartDate = confirmStartDate 
         this.confirmEndDate  = confirmEndDate
         this.extRefNo=extRefNo
         this.tab = tab
-
+        this.expectedStartDate = expectedStartDate
+        this.expectedEndDate = expectedEndDate
     }
 
 }
