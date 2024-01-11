7b5d46a9a2fefaa5604fb47e58f18d269a236b3f
Venky-2329 <118739220+Venky-2329@users.noreply.github.com>
2024-01-11 15:17:14 +0530

    trim params

--- gtstoinfor/packages/ui/src/app/sourcing-requisition/sourcing-requisition-dynamic-form.tsx
+++ gtstoinfor/packages/ui/src/app/sourcing-requisition/sourcing-requisition-dynamic-form.tsx
@@ -1658,13 +1658,13 @@ const handleFabricsfilterData = (data) => {
                                                     allowClear
                                                     showSearch
                                                     optionFilterProp="children"
-                                                    placeholder={renderTrimCodeOptions()[0]?.props.children}
+                                                    // placeholder={renderTrimCodeOptions()[0]?.props.children}
                                                     onChange={onTrimChange}
                                                     suffixIcon={trimForm.getFieldValue('trimCategory') != undefined?<SearchOutlined
                                                         onClick={trimFilterFormVisible}
                                                          style={{ fontSize: '28px', marginLeft: '-7px' }} />:<></>}
                                                 >
-                                                    {renderTrimCodeOptions()}
+                                                    {/* {renderTrimCodeOptions()} */}
                                                     {m3Trims.map((e) => {
                                                         return (
                                                             <Option key={e.m3TrimsId} value={e.m3TrimsId} trimUomId={e.uomId} trimUomName={e.uom}>
