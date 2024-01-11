bd0422da20fce6b15ef4445ff764ecee1f969974
Jaswanth <132431551+ReactJaswanth@users.noreply.github.com>
2024-01-11 22:53:12 +0530

    Orders Data Saved Succesffully

--- gtstoinfor/packages/services/common/src/app/hb-athletics/hb-athletic.service.ts
+++ gtstoinfor/packages/services/common/src/app/hb-athletics/hb-athletic.service.ts
@@ -27,18 +27,19 @@ export class HbService {
     // const transactionManager = new GenericTransactionManager(this.dataSource)
     try {
       let saved
+      const pdfData=[];
       // await transactionManager.startTransaction()
       for (const item of req.HbpoItemDetails) {
-        const match = item.style.match(/\d+/);
+        const match = item.color.match(/\d+/);
         console.log(match, "match");
         console.log(item, "item");
         // Check if a match is found and convert it to an integer
-        // const style = match ? parseInt(match[0], 10) : null;
-        const style = match
+        // const color = match ? parseInt(match[0], 10) : null;
+        const color = match
 
-        console.log(style, "style")
+        console.log(color, "color")
         for (const variant of item.HbpoItemVariantDetails) {
-          const orderData = await this.HbOrdersRepo.findOne({ where: { custPo: req.custPo, style: item.style, size: variant.size } })
+          const orderData = await this.HbOrdersRepo.findOne({ where: { custPo: req.custPo, color: item.color, size: variant.size } })
           console.log(orderData, "orderData")
           console.log(variant, "variant");
           const entity = new HbOrdersEntity();
@@ -54,13 +55,17 @@ export class HbService {
           entity.quantity = variant.quantity
           entity.unitPrice = variant.unitPrice
 
+          pdfData.push(entity)
+
+
           if (orderData) {
-            const update = await this.HbOrdersRepo.update({ custPo: req.custPo, style: item.style, size: variant.size }, {exitFactoryDate:req.exitFactoryDate,shipToAdd:req.shipToAdd,color:item.color,quantity:variant.quantity,unitPrice:variant.unitPrice})
+            const update = await this.HbOrdersRepo.update({ custPo: req.custPo, color: item.color, size: variant.size }, {exitFactoryDate:req.exitFactoryDate,shipToAdd:req.shipToAdd,style:item.style,quantity:variant.quantity,unitPrice:variant.unitPrice})
             if (!update.affected) {
               throw new Error('Update failed');
             }
           } else {
-            saved = await this.HbOrdersRepo.save(entity)
+            saved = await this.HbOrdersRepo.save(pdfData)
+            console.log(saved,"saved")
             // const savedChild = await transactionManager.getRepository(RLOrdersEntity).save(entity)
             if (!saved) {
               throw new Error('Save failed')
@@ -138,7 +143,7 @@ export class HbService {
     entity.filePath = filePath;
     entity.fileType = mimetype;
     entity.fileData = req;
-    console.log(entity.fileData, "fileData")
+    // console.log(entity.fileData, "fileData")
 
     const file = await this.HbPdfRepo.findOne({ where: { pdfFileName: filePath } });
     if (file) {
