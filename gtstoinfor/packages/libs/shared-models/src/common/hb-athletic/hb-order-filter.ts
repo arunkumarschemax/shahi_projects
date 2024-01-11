93783acd8cfed1bb4fdd7ad332d02424720c9e6f
KalahastiHygreev <hkalahasti@gmail.com>
2024-01-11 17:51:48 +0530

    order info and detail view added

--- gtstoinfor/packages/libs/shared-models/src/common/hb-athletic/hb-order-filter.ts
+++ gtstoinfor/packages/libs/shared-models/src/common/hb-athletic/hb-order-filter.ts
@@ -1,11 +1,24 @@
 
 export class HbPoOrderFilter {
     custPo?: string;
+    style?:string
+    color?:string
+    deliveryDateStartDate?:any
+    deliveryDateEndDate?:any
 
 
 
-    constructor(custPo?: string,) {
+    constructor(custPo?: string,style?:string,
+        color?:string,
+        deliveryDateStartDate?:any,
+        deliveryDateEndDate?:any
+    ) {
         this.custPo = custPo;
+        this.color = color
+        this.style = style
+        this.deliveryDateEndDate = deliveryDateEndDate
+        this.deliveryDateStartDate = deliveryDateStartDate
+        
 
     }
 }
