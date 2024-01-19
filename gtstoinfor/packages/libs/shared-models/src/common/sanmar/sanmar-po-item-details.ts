6dd65b7a6b241a5609a78e0512d1573a5b748a3c
Jaswanth <132431551+ReactJaswanth@users.noreply.github.com>
2024-01-19 15:06:37 +0530

    new changes

--- gtstoinfor/packages/libs/shared-models/src/common/sanmar/sanmar-po-item-details.ts
+++ gtstoinfor/packages/libs/shared-models/src/common/sanmar/sanmar-po-item-details.ts
@@ -2,41 +2,9 @@ import { SanmarPoItemVariant } from "./sanmar-po-item-variant";
 
 export class SanmarPoItemDetails {
 
-    poLine: string;
-    material: string;
-    color: string;
-    gender: string;
-    shortDescription:string;
-    packMethod:string;
-    vendorBookingFlag:string;
+    poStyle: string;
+    deliveryDate: string;
 
-    ppkupc:string;
-    currency:string;
-    totalQuantity:string
-
-    // materialNo: string;
-    // poItem: string;
-    // season: string;
-    // incoterms: string;
-    // contractualDeliveryDate: string;
-    // inboundPkg: string;
-    // incotermsPlace: string;
-    // handoverDate: string;
-    // mfgProcess: string;
-    // harbor: string;
-    // shipMode: string;
-    // model: string;
-    // productType: string;
-    // merchDivision: string;
-    // colorDescription: string;
-    // class: string;
-    // conceptShortDesc: string;
-    // fabricContent: string;
-    // board: string;
-    // fishWildlifeInd: string;
-    // gender: string;
-    // downFeatherInd: string;
-    // fabrication: string;
     SanmarpoItemVariantDetails: SanmarPoItemVariant[]
 
 }
