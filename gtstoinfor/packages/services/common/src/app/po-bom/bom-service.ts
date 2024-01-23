6ad2a30b9ac8a014de74b04e64dee838d752a65d
Renukaattada <renukaattata21@gmail.com>
2024-01-23 11:09:19 +0530

    #changes done in bom

--- gtstoinfor/packages/services/common/src/app/po-bom/bom-service.ts
+++ gtstoinfor/packages/services/common/src/app/po-bom/bom-service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { StyleEntity } from "./entittes/style-entity";
-import { Repository } from "typeorm";
+import { DataSource, Repository } from "typeorm";
 import { CommonResponseModel } from "@project-management-system/shared-models";
 import { StyleDto } from "./dto/style-dto";
 import { BomEntity } from "./entittes/bom-entity";
@@ -10,6 +10,7 @@ import { StyleRepo } from "./dto/style-repo";
 import { BomRepo } from "./dto/bom-repo";
 import { StyleComboRepo } from "./dto/style-combo-repo";
 import { BomDto } from "./dto/bom-dto";
+import { StyleComboDto } from "./dto/style-combo-dto";
 
 @Injectable()
 export class BomService{
@@ -17,7 +18,9 @@ export class BomService{
 
     private StyleRepo: StyleRepo,
     private bomRepo: BomRepo,
-    private styleComboRepo:StyleComboRepo
+    private styleComboRepo:StyleComboRepo,
+    private dataSource: DataSource,
+
     ){ }
     async createBom(req:StyleDto):Promise<CommonResponseModel>{
         try{
@@ -87,6 +90,36 @@ export class BomService{
             }else{
                 return new CommonResponseModel(false,1,'No Data Found',[])
             }
+        }
+        catch(err){
+            throw err
+        }
+    }
+
+
+    async getAll():Promise<CommonResponseModel>{
+        try{
+            const query= 'SELECT s.id as styeleId,s.style, s.style_name AS styleName,s.season,s.exp_no AS expNo,b.id AS bomId,b.item_name AS itemName,b.DESCRIPTION,b.im_code AS imCode,b.item_type AS itemType ,sc.id AS styleComboId,sc.bom_id AS bomId,sc.style_id AS sstyleId,sc.combination,sc.primary_color AS primaryColor,sc.secondary_color AS secondaryColor,sc.logo_color AS logoColor FROM styles s  LEFT JOIN bom b ON b.style_id=s.id LEFT JOIN style_combos sc ON sc.bom_id=b.id'
+            const result = await this.dataSource.query(query)
+            const combo=[]
+            const allStyleData =new Map<number, StyleDto>()
+            const comboStyle = new Map<number, StyleComboDto>()
+            if(result.length >0){
+                for(const rec of result){
+                    if(!allStyleData.has(rec.styeleId)){
+                        allStyleData.set(rec.styeleId,new StyleDto(rec.style,rec.styleName,rec.season,rec.expNo,[]))
+                    }
+                    allStyleData.get(rec.styeleId).bomdto.push(new BomDto(rec.itemName,rec.description,rec.imCode,rec.itemType,rec.use,combo))
+                    // if(!comboStyle.has(rec.bomId)){
+                    //    const data= comboStyle.set(rec.bomId,new StyleComboDto(rec.combination,rec.primaryColor,rec.secondaryColor,rec.logoColor))
+                    // }
+                }
+                const responsemodel:StyleDto[]=[]
+                allStyleData.forEach((rec =>responsemodel.push(rec)))
+             return new CommonResponseModel(true,1,'Data retrived',responsemodel)
+            }
+
+
         }
         catch(err){
             throw err
