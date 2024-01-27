3962e8f97f51c60f4ae85a831998e378896d665b
dileepraghumajji88 <dileepraghumajji88@gmail.com>
2024-01-27 11:45:27 +0530

    #changes

--- gtstoinfor/packages/ui/src/app/trims/trim-columns.tsx
+++ gtstoinfor/packages/ui/src/app/trims/trim-columns.tsx
@@ -6,49 +6,49 @@ import React from "react";
 
 export function TrimColumns() {
 
-    return(
+    return (
         <Card>
-         <div style={{ overflowX: 'auto', maxWidth: '100%'}}>
-        <table style={{borderCollapse:'collapse',borderBlockColor:'black',width:'100%'}} border={1} cellSpacing="0" cellPadding='0'>
-        <tr>
-            <th style={{width:' 10%'}}>ITEM#</th>
-            <th style={{width:'10%'}}>STYLE#</th>
-            <th style={{width:'10%'}}>TRIM#</th>
-            <th style={{width:'10%'}}>IM#</th>
-            <th style={{width:'10%'}}>Desc</th>
-            <th style={{width:'10%'}}>USED AT</th>
-            <th style={{width:'10%'}}>GMT CLR</th>
-            <th style={{width:'10%'}}>INTERLINING CLR</th>
-            <th style={{width:'10%'}}>REQ</th>
-            <th style={{width:'10%'}}>Season#</th>
-            <th style={{width:'10%'}}>Material Description</th>
-            <th style={{width:'10%'}}>Garment Colour-Code</th>
-            <th style={{width:'10%'}}>Valentine's Patch Label Color</th>
-            <th style={{width:'10%'}}>Quantity in Pcs</th>
-            <th style={{width:'10%'}}>Garment Colour + code</th>
-            <th style={{width:'10%'}}>Tape Colour</th>
-            <th style={{width:'10%'}}>QTY in YardS</th>
-            <th style={{width:'10%'}}>Balaji Super Spandex Ref#</th>
-            <th style={{width:'10%'}}>Consumtion</th>
-            <th style={{width:'10%'}}>Garment Colors</th>
-            <th style={{width:'10%'}}>Elastic Color</th>
-            <th style={{width:'10%'}}>Req In Mtrs</th>
-            <th style={{width:'10%'}}>Trim Description</th>
-            <th style={{width:'10%'}}>Snap size</th>
-            <th style={{width:'10%'}}>Snap Color</th>
-            <th style={{width:'10%'}}>Req in Gross(with set)</th>
-            <th style={{width:'10%'}}>Supplier</th>
-            <th style={{width:'10%'}}>Golf# 3HT Label</th>
-            <th style={{width:'10%'}}>Swoosh Color</th>
-            <th style={{width:'10%'}}>QTY in Pcs</th>
+            <div style={{ overflowX: 'auto', maxWidth: '100%' }}>
+                <table style={{ borderCollapse: 'collapse', borderBlockColor: 'black', width: '100%' }} border={1} cellSpacing="0" cellPadding='0'>
+                    <tr>
+                        <th style={{ width: ' 10%' }}>ITEM#</th>
+                        <th style={{ width: '10%' }}>STYLE#</th>
+                        <th style={{ width: '10%' }}>TRIM#</th>
+                        <th style={{ width: '10%' }}>IM#</th>
+                        <th style={{ width: '10%' }}>Desc</th>
+                        <th style={{ width: '10%' }}>USED AT</th>
+                        <th style={{ width: '10%' }}>GMT CLR</th>
+                        <th style={{ width: '10%' }}>INTERLINING CLR</th>
+                        <th style={{ width: '10%' }}>REQ</th>
+                        <th style={{ width: '10%' }}>Season#</th>
+                        <th style={{ width: '10%' }}>Material Description</th>
+                        <th style={{ width: '10%' }}>Garment Colour-Code</th>
+                        <th style={{ width: '10%' }}>Valentine's Patch Label Color</th>
+                        <th style={{ width: '10%' }}>Quantity in Pcs</th>
+                        <th style={{ width: '10%' }}>Garment Colour + code</th>
+                        <th style={{ width: '10%' }}>Tape Colour</th>
+                        <th style={{ width: '10%' }}>QTY in YardS</th>
+                        <th style={{ width: '10%' }}>Balaji Super Spandex Ref#</th>
+                        <th style={{ width: '10%' }}>Consumtion</th>
+                        <th style={{ width: '10%' }}>Garment Colors</th>
+                        <th style={{ width: '10%' }}>Elastic Color</th>
+                        <th style={{ width: '10%' }}>Req In Mtrs</th>
+                        <th style={{ width: '10%' }}>Trim Description</th>
+                        <th style={{ width: '10%' }}>Snap size</th>
+                        <th style={{ width: '10%' }}>Snap Color</th>
+                        <th style={{ width: '10%' }}>Req in Gross(with set)</th>
+                        <th style={{ width: '10%' }}>Supplier</th>
+                        <th style={{ width: '10%' }}>Golf# 3HT Label</th>
+                        <th style={{ width: '10%' }}>Swoosh Color</th>
+                        <th style={{ width: '10%' }}>QTY in Pcs</th>
 
-        </tr>
-    </table>
-</div>
+                    </tr>
+                </table>
+            </div>
 
-      
-      </Card>
-        
+
+        </Card>
+
     )
 
 
---
+++ gtstoinfor/packages/ui/src/app/trims/wash-care-label/washcare-label.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function WashCareLabel() {
+  return (
+    <div>washcare-label</div>
+  )
+}
