252e23fc23e71f7f54049181c225274e06ec2078
Renukaattada <renukaattata21@gmail.com>
2024-01-29 15:15:15 +0530

    #transport amount added in po detailed view

--- gtstoinfor/packages/ui/src/app/purchase-order2/purchase-order-details-view.tsx
+++ gtstoinfor/packages/ui/src/app/purchase-order2/purchase-order-details-view.tsx
@@ -142,6 +142,12 @@ export const PurchaseOrderDetailsView = (props:PoDetailViewPagesProps) => {
       },
     },
     {
+      title:'Transportation Amount',
+      key:'transportation',
+      dataIndex:'transportation'
+
+    },
+    {
       title: 'Tax %',
       key: 'Tax %',
       dataIndex: 'taxPercentage',
