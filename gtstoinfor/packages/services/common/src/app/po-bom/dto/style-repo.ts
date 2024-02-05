09df49000a3235a1974a42821feb8da36cff4a49
Bali Rajeswari <balirajeswari11@gmail.com>
2024-02-05 10:33:10 +0530

    added item in getbominfobystyle api

--- gtstoinfor/packages/services/common/src/app/po-bom/dto/style-repo.ts
+++ gtstoinfor/packages/services/common/src/app/po-bom/dto/style-repo.ts
@@ -6,6 +6,7 @@ import { StyleDto } from "./style-dto";
 import { BomEntity } from "../entittes/bom-entity";
 import { StyleComboEntity } from "../entittes/style-combo-entity";
 import { StyleNumberDto } from "./style-number-dto";
+import { DpomEntity } from "../../dpom/entites/dpom.entity";
 
 
 @Injectable()
@@ -23,9 +24,10 @@ export class StyleRepo extends Repository<StyleEntity> {
 
     async getBomInfoAgainstStyle(req:StyleNumberDto):Promise<any[]>{
         const query = this.createQueryBuilder('sty')
-        .select(`sty.id as styleId,sty.style,sty.style_name,bom.item_name,bom.description,bom.im_code,bom.item_type,bom.use,stco.combination,stco.primary_color,stco.secondary_color,stco.logo_color,bom.id as bomId,stco.id as styleComboId,stco.color,bom.uom,bom.qty`)
+        .select(`sty.id as styleId,sty.style,sty.style_name,bom.item_name,bom.description,bom.im_code,bom.item_type,bom.use,stco.combination,stco.primary_color,stco.secondary_color,stco.logo_color,bom.id as bomId,stco.id as styleComboId,stco.color,bom.uom,bom.qty,dpom.po_number,dpom.item`)
         .leftJoin(BomEntity,'bom',`bom.style_id = sty.id`)
         .leftJoin(StyleComboEntity,'stco',`stco.bom_id = bom.id`)
+        .leftJoin(DpomEntity,'dpom',`dpom.style_number = sty.style`)
         .where(`sty.style = '${req.style}' and item_name='${req.trimName}'`)
         return await query.getRawMany()
 
