947b43021e645e9ac8b18dd3024babcb7e0249cb
Jaswanth <132431551+ReactJaswanth@users.noreply.github.com>
2024-02-06 15:38:40 +0530

    Accepted changes

--- gtstoinfor/packages/ui/src/app/centric/centric-order-acceptance-view.tsx
+++ gtstoinfor/packages/ui/src/app/centric/centric-order-acceptance-view.tsx
@@ -788,7 +788,7 @@ export function CentricOrderAcceptanceGrid() {
                 onClick={() => createCOLine(record, index)}
                 disabled={record.status === 'INPROGRESS' ? true : !isEnabled}
               >
-                {record.status === 'INPROGRESS' ? "Accept" : "Accept"}
+                {record.status === 'INPROGRESS' ? "Accepted" : "Accept"}
               </Button>
             ),
             props: {
