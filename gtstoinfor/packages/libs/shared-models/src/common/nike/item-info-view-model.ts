042037f6ae5cc911f394f5608dd409643bcdf96c
Bali Rajeswari <balirajeswari11@gmail.com>
2024-02-09 10:23:33 +0530

    item info view model

--- gtstoinfor/packages/libs/shared-models/src/common/nike/item-info-view-model.ts
+++ gtstoinfor/packages/libs/shared-models/src/common/nike/item-info-view-model.ts
@@ -29,3 +29,48 @@ export class RegionModel{
     this.geoCode = geoCode
     }
 }
+
+export class ItemInfo{
+    id:number;
+    item:string;
+    poLineItemNumber:number;
+    poNumber:number;
+    totalItemQty:number;
+    styleInfo:StyleInfo[]
+
+    constructor(id:number,item:string,poLineItemNumber:number,poNumber:number,totalItemQty:number,styleInfo:StyleInfo[]){
+        this.id = id
+        this.item = item
+        this.poLineItemNumber = poLineItemNumber
+        this.poNumber = poNumber
+        this.totalItemQty = totalItemQty
+        this.styleInfo = styleInfo
+    }
+}
+
+export class StyleInfo{
+    styleNumber:string;
+    regionInfo:RegionInfo[]
+    constructor(styleNumber:string,regionInfo:RegionInfo[]){
+        this.styleNumber = styleNumber
+        this.regionInfo = regionInfo
+    }
+}
+
+export class RegionInfo{
+    geoCode :string;
+    destinationInfo:DestinationInfo[]
+    constructor(geoCode:string,destinationInfo:DestinationInfo[]){
+        this.geoCode = geoCode
+        this.destinationInfo = destinationInfo
+    }
+}
+
+export class DestinationInfo{
+    destinationCountryCode:string;
+    destinationCountry: string;
+    constructor(destinationCountryCode:string,destinationCountry: string){
+        this.destinationCountry = destinationCountry
+        this.destinationCountryCode = destinationCountryCode
+    }
+}
