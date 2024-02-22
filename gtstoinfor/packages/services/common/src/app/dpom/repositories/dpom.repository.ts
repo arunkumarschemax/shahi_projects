ae40c4b24fe513c724ff14f7ba4df5cb19086a2e
saiprakashkatiki <saiprakash@schemaxtech.com>
2024-02-22 12:04:39 +0530

    PO change report

--- gtstoinfor/packages/services/common/src/app/dpom/repositories/dpom.repository.ts
+++ gtstoinfor/packages/services/common/src/app/dpom/repositories/dpom.repository.ts
@@ -185,7 +185,7 @@ export class DpomRepository extends Repository<DpomEntity> {
             .select(`o.po_number, o.po_line_item_number,  o.item, o.factory,o.document_date,o.style_number,o.product_code,o.color_desc,o.destination_country,o.ogac,o.gac,o.item_text,
             o.size_description,o.customer_order,o.schedule_line_item_number, o.total_item_qty, o.dpom_item_line_status, od.created_at, od.old_val, od.new_val, od.odVersion`)
             .leftJoin(DpomDifferenceEntity, 'od', 'od.po_number = o.po_number AND od.po_line_item_number = o.po_line_item_number AND od.schedule_line_item_number = o.schedule_line_item_number')
-            .where(` od.column_name='gac' `)
+            .where(` od.column_name='gac' AND DATE(od.created_at) = CURDATE()`)
             .groupBy(` o.po_and_line`)
             .orderBy(' od.created_at', 'DESC')
         return await query.getRawMany();
@@ -232,7 +232,7 @@ export class DpomRepository extends Repository<DpomEntity> {
         const query = this.createQueryBuilder('dpom')
             .select(`dpom.po_number AS purchaseOrderNumber,dpom.created_at,dpom.item,dpom.factory,dpom.product_code AS productCode,dpom.ogac AS OGAC,dpom.style_number AS styleNumber,dpom.destination_country AS destinationCountry,dpom.color_desc,dpom.size_description,dpom.gac AS GAC,dpom.total_item_qty AS totalItemQty,dpom.item_vas_text AS itemVasText,dpom.po_and_line ,dpom.po_line_item_number AS poLineItemNumber, dpom.schedule_line_item_number, dpom.total_item_qty, dpom.color_desc AS colorDesc, dpom.dpom_item_line_status, od.created_at, od.old_val, od.new_val, (od.new_val - od.old_val) AS Diff , od.odVersion`)
             .leftJoin(DpomDifferenceEntity, 'od', 'od.po_number = dpom.po_number AND od.po_line_item_number = dpom.po_line_item_number AND od.schedule_line_item_number = dpom.schedule_line_item_number')
-            .where(` od.column_name='item_vas_text' AND (od.po_number, od.od_version) IN (
+            .where(` od.column_name='item_vas_text' AND DATE(od.created_at) = CURDATE() AND (od.po_number, od.od_version) IN (
                 SELECT po_number, MAX(od_version) AS max_version FROM dpom_diff GROUP BY po_number)`)
             .groupBy(' dpom.po_and_line')
             .orderBy(' od.created_at')
