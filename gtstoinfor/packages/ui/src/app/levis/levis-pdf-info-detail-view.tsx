e36ef5c113d5fc8a4a088e595e65a40b8f35912e
Jaswanth <132431551+ReactJaswanth@users.noreply.github.com>
2024-02-23 11:13:30 +0530

    pdf view changes

--- gtstoinfor/packages/ui/src/app/levis/levis-pdf-info-detail-view.tsx
+++ gtstoinfor/packages/ui/src/app/levis/levis-pdf-info-detail-view.tsx
@@ -37,14 +37,14 @@ const LevisPdfInfoDetailView = (
             <table className='ta-b' style={{ width: '100%' }} >
                 <tr className='ta-b'>
                 <th className='ta-b'>PO NUMBER</th>
-                    <th className='ta-b'>TRANSMODE</th>
+                    {/* <th className='ta-b'>TRANSMODE</th> */}
                     <th className='ta-b'>DELIVERY ADDRESS</th>
                     <th className='ta-b'>CURRENCY</th>
                 </tr>
 
                 <tr className='ta-b'>
                     <td className='ta-b'>{parsedData?.poNumber}</td>
-                    <td className='ta-b'>{parsedData?.transMode}</td>
+                    {/* <td className='ta-b'>{parsedData?.transMode}</td> */}
                     <td className='ta-b'>{parsedData?.deliveryAddress}</td>
                     <td className='ta-b'>{parsedData?.currency}</td>
                 </tr>
@@ -55,25 +55,27 @@ const LevisPdfInfoDetailView = (
                             <th></th>
                             <th className='ta-b'>PO LINE</th>
                             <th className='ta-b'>MATERIAL</th>
-                            <th className='ta-b'>TOTAL UNIT PRICE</th>
-                            <th className='ta-b'>ORIGINAL DATE</th>
+                            {/* <th className='ta-b'>TOTAL UNIT PRICE</th> */}
+                            <th className='ta-b'>TRANS MODE</th>
+                            <th className='ta-b'>EX FACTORY DATE</th>
                         </tr>
                         <tr className='ta-b'>
                             <td></td>
                             <td className='ta-b'>{i.poLine}</td>
                             <td className='ta-b'>{i.material}</td>
-                            <td className='ta-b'>{i.totalUnitPrice}</td>
-                            <td className='ta-b'>{i.originalDate}</td>
+                            {/* <td className='ta-b'>{i.totalUnitPrice}</td> */}
+                            <td className='ta-b'>{i.transMode}</td>
+                            <td className='ta-b'>{i.exFactoryDate}</td>
                         </tr>
                         <tr className='ta-b'>
                             <th></th>
                             <th></th>
 
-                            <th className='ta-b'>PRODUCT</th>
+                            <th className='ta-b'>ITEM NO</th>
                             <th className='ta-b'>SIZE</th>
-                            <th className='ta-b'>UPC</th>
+                            {/* <th className='ta-b'>UPC</th>
                             <th className='ta-b'>PLANNED EX FACTORY DATE</th>
-                            <th className='ta-b'>EX FACTORY DATE</th>
+                            <th className='ta-b'>EX FACTORY DATE</th> */}
                             <th className='ta-b'>QUANTITY</th>
                             <th className='ta-b'>UNIT PRICE</th>
          
@@ -83,11 +85,11 @@ const LevisPdfInfoDetailView = (
                                 return <tr>
                                     <td></td>
                                     <td></td>
-                                    <td className='ta-b'>{j.product}</td>
+                                    <td className='ta-b'>{j.itemNo}</td>
                                     <td className='ta-b'>{j.size}</td>
-                                    <td className='ta-b'>{j.upc}</td>
+                                    {/* <td className='ta-b'>{j.upc}</td>
                                     <td className='ta-b'>{j.plannedExFactoryDate}</td>
-                                    <td className='ta-b'>{j.exFactoryDate}</td>
+                                    <td className='ta-b'>{j.exFactoryDate}</td> */}
                                     <td className='ta-b'>{j.quantity}</td>
                                     <td className='ta-b'>{j.unitPrice}</td>
                                 </tr>
