627f677f3444fd65300b02fad8d005c54aa4f301
Jaswanth <132431551+ReactJaswanth@users.noreply.github.com>
2024-03-11 14:30:41 +0530

    repo changes

--- gtstoinfor/packages/services/common/src/app/levis/repositories/levis-orders.repo.ts
+++ gtstoinfor/packages/services/common/src/app/levis/repositories/levis-orders.repo.ts
@@ -87,23 +87,6 @@ export class LevisOrdersRepository extends Repository<LevisOrdersEntity> {
         return await query.getRawMany()
     }
 
-    // async getSplitOrderComparisionData(req?: LevisOrderFilter): Promise<any[]> {
-    //     const query = this.createQueryBuilder('o')
-    //         .select(`*`)
-    //     if (req.poNumber !== undefined) {
-    //         query.andWhere(`o.po_number ='${req.poNumber}'`)
-    //     }
-    //     else if (req.splitPo !== undefined) {
-    //         query.andWhere(`o.split_po ='${req.splitPo}'`)
-    //     }
-    //     else if (req.splitPoTotalQuantity !== undefined) {
-    //         query.andWhere(`o.split_po_total_quantity ='${req.splitPoTotalQuantity}'`)
-    //     }
-
-
-    //     return await query.getRawMany()
-    // }
-
     async getSplitOrderComparisionData(req: LevisOrderFilter): Promise<any[]> {
         const query = this.createQueryBuilder('co')
             .select(`co.po_number,co.po_line,co.size, co.split_po,co.total_quantity,co.split_po_total_quantity,co.id`)
@@ -112,20 +95,13 @@ export class LevisOrdersRepository extends Repository<LevisOrdersEntity> {
         if (req.poNumber !== undefined) {
             query.andWhere(`co.po_number ='${req.poNumber}'`)
         }
-        // else if (req.splitPo !== undefined) {
-        //     query.andWhere(`o.split_po ='${req.splitPo}'`)
-        // }
-        // else if (req.splitPoTotalQuantity !== undefined) {
-        //     query.andWhere(`o.split_po_total_quantity ='${req.splitPoTotalQuantity}'`)
-        // }
-
-
+        
         return await query.getRawMany();
     }
 
 
-      async getItemsNo(req:any): Promise<any> {
-       // console.log(req,"jjjjjjj")
+    async getItemsNo(req: any): Promise<any> {
+        // console.log(req,"jjjjjjj")
         const query = this.createQueryBuilder('o')
             .select(`lc.item_no,lc.co_date,lc.co_number`)
             .leftJoin(`LevisCOLineEntity`, 'lc', 'lc.po_number = o.po_number')
