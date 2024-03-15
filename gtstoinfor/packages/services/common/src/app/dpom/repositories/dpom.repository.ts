f8de53b62253069325bd77758eeafc67352b9939
saiprakashkatiki <saiprakash@schemaxtech.com>
2024-03-15 17:54:42 +0530

    change report 1 week data

--- gtstoinfor/packages/services/common/src/app/dpom/repositories/dpom.repository.ts
+++ gtstoinfor/packages/services/common/src/app/dpom/repositories/dpom.repository.ts
@@ -86,7 +86,7 @@ export class DpomRepository extends Repository<DpomEntity> {
         const query = this.createQueryBuilder('dpom')
             .select(`dpom.po_number,dpom.item,dpom.factory,dpom.document_date,dpom.product_code AS productCode,dpom.ogac AS OGAC,dpom.style_number AS styleNumber,dpom.destination_country AS desCtry,dpom.color_desc,dpom.size_description,dpom.gac AS GAC,dpom.total_item_qty AS totalItemQty,dpom.item_text,dpom.po_and_line ,dpom.po_line_item_number, dpom.schedule_line_item_number, dpom.total_item_qty, dpom.dpom_item_line_status, od.created_at, od.old_val, od.new_val, (od.new_val - od.old_val) AS Diff , od.odVersion`)
             .leftJoin(DpomDifferenceEntity, 'od', 'od.po_number = dpom.po_number AND od.po_line_item_number = dpom.po_line_item_number AND od.schedule_line_item_number = dpom.schedule_line_item_number')
-            .where(` od.column_name='size_qty' AND od.created_at >= DATE_SUB(CURDATE(), INTERVAL 3 DAY) `)
+            .where(` od.column_name='size_qty' AND od.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) `)
             .orderBy(` od.created_at`, 'DESC')
         if (req && req.poandLine !== undefined) {
             query.andWhere(`dpom.po_and_line ='${req.poandLine}'`)
@@ -126,7 +126,7 @@ export class DpomRepository extends Repository<DpomEntity> {
         const query = this.createQueryBuilder('dpom')
             .select(`dpom.po_number,dpom.item,dpom.factory,dpom.document_date,dpom.product_code AS productCode,dpom.ogac AS OGAC,dpom.style_number AS styleNumber,dpom.destination_country AS desCtry,dpom.color_desc,dpom.size_description,dpom.gac AS GAC,dpom.total_item_qty AS totalItemQty,dpom.item_text,dpom.po_and_line ,dpom.po_line_item_number, dpom.schedule_line_item_number, dpom.total_item_qty, dpom.dpom_item_line_status, od.created_at, od.old_val, od.new_val, (od.new_val - od.old_val) AS Diff , od.odVersion`)
             .leftJoin(DpomDifferenceEntity, 'od', 'od.po_number = dpom.po_number AND od.po_line_item_number = dpom.po_line_item_number AND od.schedule_line_item_number = dpom.schedule_line_item_number')
-            .where(` od.display_name='itemText' AND od.created_at >= DATE_SUB(CURDATE(), INTERVAL 3 DAY) `)
+            .where(` od.display_name='itemText' AND od.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) `)
             .orderBy(` od.created_at`, 'DESC')
         if (req && req.poandLine !== undefined) {
             query.andWhere(`dpom.po_and_line ='${req.poandLine}'`)
@@ -168,7 +168,7 @@ export class DpomRepository extends Repository<DpomEntity> {
             od.schedule_line_item_number = o.schedule_line_item_number `)
             .leftJoin(FobEntity, 'fm', `fm.style_number = o.style_number AND fm.color_code = SUBSTRING_INDEX(o.product_code, '-', -1) AND fm.size_description = o.size_description`)
             .where(` od.display_name IN ('grossPriceFOB','trCoNetIncludingDisc','trCoNetIncludingDiscCurrencyCode',
-            'shahiOfferedPricefromMasterFile','shahicurrencyCodeMasterFile','legalPoPrice','legalPoCurrency') AND od.created_at >= DATE_SUB(CURDATE(), INTERVAL 3 DAY)`)
+            'shahiOfferedPricefromMasterFile','shahicurrencyCodeMasterFile','legalPoPrice','legalPoCurrency') AND od.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK)`)
             .groupBy('o.po_number, o.item')
             .orderBy(' od.created_at', 'DESC')
         // if (req.poandLine !== undefined) {
@@ -198,7 +198,7 @@ export class DpomRepository extends Repository<DpomEntity> {
             .select(`o.po_number, o.po_line_item_number,  o.item, o.factory,o.document_date,o.style_number,o.product_code,o.color_desc,o.destination_country,o.ogac,o.gac,o.item_text,
             o.size_description,o.customer_order,o.schedule_line_item_number, o.total_item_qty, o.dpom_item_line_status, od.created_at, od.old_val, od.new_val, od.odVersion`)
             .leftJoin(DpomDifferenceEntity, 'od', 'od.po_number = o.po_number AND od.po_line_item_number = o.po_line_item_number AND od.schedule_line_item_number = o.schedule_line_item_number')
-            .where(` od.column_name='gac' AND od.created_at >= DATE_SUB(CURDATE(), INTERVAL 3 DAY)`)
+            .where(` od.column_name='gac' AND od.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK)`)
             .groupBy(` o.po_and_line`)
             .orderBy(' od.created_at', 'DESC')
         return await query.getRawMany();
@@ -218,7 +218,7 @@ export class DpomRepository extends Repository<DpomEntity> {
             .select(`o.po_number, o.po_line_item_number, o.schedule_line_item_number, o.item, o.factory,o.document_date,o.style_number,o.product_code,o.color_desc,o.destination_country,o.ogac,o.gac,o.item_text,
             o.size_description,o.customer_order, o.total_item_qty, o.dpom_item_line_status, od.created_at, od.old_val, od.new_val, od.odVersion`)
             .leftJoin(DpomDifferenceEntity, 'od', 'od.po_number = o.po_number AND od.po_line_item_number = o.po_line_item_number AND od.schedule_line_item_number = o.schedule_line_item_number')
-            .where(` od.column_name='unit' AND od.created_at >= DATE_SUB(CURDATE(), INTERVAL 3 DAY) `)
+            .where(` od.column_name='unit' AND od.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) `)
         return await query.getRawMany();
     }
 
@@ -245,7 +245,7 @@ export class DpomRepository extends Repository<DpomEntity> {
         const query = this.createQueryBuilder('dpom')
             .select(`dpom.po_number AS purchaseOrderNumber,dpom.created_at,dpom.item,dpom.factory,dpom.product_code AS productCode,dpom.ogac AS OGAC,dpom.style_number AS styleNumber,dpom.destination_country AS destinationCountry,dpom.color_desc,dpom.size_description,dpom.gac AS GAC,dpom.total_item_qty AS totalItemQty,dpom.item_vas_text AS itemVasText,dpom.po_and_line ,dpom.po_line_item_number AS poLineItemNumber, dpom.schedule_line_item_number, dpom.color_desc AS colorDesc, dpom.item_vas_pdf, dpom.dpom_item_line_status, od.created_at, od.old_val, od.new_val, od.odVersion`)
             .leftJoin(DpomDifferenceEntity, 'od', 'od.po_number = dpom.po_number AND od.po_line_item_number = dpom.po_line_item_number AND od.schedule_line_item_number = dpom.schedule_line_item_number')
-            .where(` od.column_name='item_vas_text' AND od.created_at >= DATE_SUB(CURDATE(), INTERVAL 3 DAY) AND (od.po_number, od.od_version) IN (
+            .where(` od.column_name='item_vas_text' AND od.created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK) AND (od.po_number, od.od_version) IN (
                 SELECT po_number, MAX(od_version) AS max_version FROM dpom_diff GROUP BY po_number)`)
             .groupBy(' dpom.po_and_line')
             .orderBy(' od.created_at')
