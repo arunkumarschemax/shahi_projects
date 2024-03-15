ad4571136d0231d07431e6f7db1d395fcac8289e
Rahulsingarapu <rahul.s@schemaxtech.com>
2024-03-16 00:19:24 +0530

    #config changes

--- gtstoinfor/packages/libs/shared-services/config.ts
+++ gtstoinfor/packages/libs/shared-services/config.ts
@@ -4,8 +4,8 @@ export const config = {
     whatsapp_notification_url: 'http://206.189.138.212:3000',
     whatsapp_broadcast_url: 'http://206.189.138.212:3232/api',
 
-    //  APP_PROJECT_MANAGEMENT_URL: "http://165.22.220.143:9008/api",
-    APP_PROJECT_MANAGEMENT_URL: "http://localhost:9008/api",
+     APP_PROJECT_MANAGEMENT_URL: "http://165.22.220.143:9008/api",
+    // APP_PROJECT_MANAGEMENT_URL: "http://localhost:9008/api",
 
 
     APP_IAM_SERVER_URL: "https://ums-backend.shahiapps.in/",
