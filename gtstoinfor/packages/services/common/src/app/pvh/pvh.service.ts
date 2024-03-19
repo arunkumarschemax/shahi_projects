03b4deb2a7fb65a8c0a2133602e7f38f556ff392
naveenmaddula1999 <113412159+naveenmaddula1999@users.noreply.github.com>
2024-03-19 15:26:33 +0530

    changes

--- gtstoinfor/packages/services/common/src/app/pvh/pvh.service.ts
+++ gtstoinfor/packages/services/common/src/app/pvh/pvh.service.ts
@@ -185,7 +185,7 @@ export class PVHService {
         if (!sizeDateMap.has(`${rec.po_line},${rec.po_number},${rec.delivery_date},${rec.color}`)) {
           sizeDateMap.set(
             `${rec.po_line},${rec.po_number},${rec.delivery_date},${rec.color}`,
-            new PvhOrderDataModel(rec.id, rec.po_number, rec.delivery_address, rec.transmode, rec.currency, rec.po_line, rec.material, rec.total_unit_price, rec.original_date, rec.status,rec.delivery_date,rec.buyer_name, [])
+            new PvhOrderDataModel(rec.id, rec.po_number, rec.delivery_address, rec.trans_mode, rec.currency, rec.po_line, rec.material, rec.total_unit_price, rec.original_date, rec.status,rec.delivery_date,rec.buyer_name, [])
           );
 
         }
