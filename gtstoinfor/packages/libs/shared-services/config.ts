c30c4b59fa4eff6ee45c5e469eed0e0370212c3d
arunkumar silla <arunkumars@schemaxtech.com>
2024-04-06 06:18:30 +0000

    Update config.ts

--- gtstoinfor/packages/libs/shared-services/config.ts
+++ gtstoinfor/packages/libs/shared-services/config.ts
@@ -5,7 +5,7 @@ export const config = {
     whatsapp_broadcast_url: 'http://206.189.138.212:3232/api',
 
     
-     APP_PROJECT_MANAGEMENT_URL: "https://levis-backend.shahiapps.in/api",
+     APP_PROJECT_MANAGEMENT_URL: "http://139.59.23.204:5000/api",
     //  APP_PROJECT_MANAGEMENT_URL: "http://localhost:9009/api",
 
 
