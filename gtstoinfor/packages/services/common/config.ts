cd1288ff1a56e7ba5972444c0e40751b67084d2f
arunkumar silla <arunkumars@schemaxtech.com>
2024-04-06 06:17:12 +0000

    Update config.ts

--- gtstoinfor/packages/services/common/config.ts
+++ gtstoinfor/packages/services/common/config.ts
@@ -6,10 +6,10 @@ export const appConfig = {
     },
     database: {
         type: process.env[`APP_DB_TYPE`] || 'mysql',
-        host: process.env[`APP_DB_HOST`] || '165.22.220.143',
+        host: process.env[`APP_DB_HOST`] || '139.59.23.204',
         port: parseInt(process.env[`APP_DB_PORT`]) || 3306,
-        username: process.env[`APP_DB_USER`] || 'ramakrishna',
-        password: process.env[`APP_DB_PASS`] || 'Schemax@23',
+        username: process.env[`APP_DB_USER`] || 'test',
+        password: process.env[`APP_DB_PASS`] || 'test@123',
         dbName: process.env[`APP_DB_DBNAME`] || 'shahi_levis',
         poolLimit: parseInt(process.env[`APP_DB_POOL_LIMIT`]) || 50
     },
