417acc6877ae8613105463d3194eee832a80ca50
arunkumar silla <arunkumars@schemaxtech.com>
2024-04-10 07:30:05 +0000

    Update config.ts

--- gtstoinfor/packages/libs/shared-services/config.ts
+++ gtstoinfor/packages/libs/shared-services/config.ts
@@ -5,14 +5,14 @@ export const config = {
     whatsapp_broadcast_url: 'http://206.189.138.212:3232/api',
 
     
-     APP_PROJECT_MANAGEMENT_URL: "http://159.65.148.229:5000/api",
+     APP_PROJECT_MANAGEMENT_URL: "http://68.183.86.255:5000/api",
     //  APP_PROJECT_MANAGEMENT_URL: "http://localhost:9009/api",
 
 
     APP_IAM_SERVER_URL: "https://ums-backend.shahiapps.in/",
     APP_IAM_CLIENT_ID: "225ba80a-3678-4a37-891e-4a55079df157",
     // file_upload_path: "http://165.22.220.143/levis/gtstoinfor/dist/packages/services/common/"
-    file_upload_path: "http://165.22.220.143/levis/gtstoinfor/upload_files/"
+    file_upload_path: "http://68.183.86.255/automation/gtstoinfor/upload_files/"
 
     
 }
