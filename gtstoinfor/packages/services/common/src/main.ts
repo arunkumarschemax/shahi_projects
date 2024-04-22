8ca6c5c7661ff240b854baf0dfdd0f946159a90b
arunkumar silla <arunkumars@schemaxtech.com>
2024-04-22 10:53:48 +0000

    Update main.ts

--- gtstoinfor/packages/services/common/src/main.ts
+++ gtstoinfor/packages/services/common/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   }));
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = Number(2000);
+  const port = Number(100);
 
   const options = new DocumentBuilder()
     .setTitle('Migration System')
